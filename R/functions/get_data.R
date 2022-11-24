230ab866bec9466092bf01873fba5865f50c6347
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2022-11-24 10:40:35 +0100

    modify fc fo loading data

--- R/functions/get_data.R
+++ R/functions/get_data.R
@@ -1,10 +1,7 @@
 #' @title Get pollen data from data assembly
 #' @description Use data assembly derived from RFossilPol
-#' @return Data assembly 
-
-get_data <- function(file) {
-  
-  readr::read_rds(file) %>% 
-    pluck("data") 
-      
+#' @return Data assembly
+get_data <- function(path) {
+  get_file_from_path(path) %>%
+    purrr::pluck("data")
 }
