08f39c6e7410123a6990c6df7a74c4ca3aa9581c
Vivian_A_F <vivian.felde@uib.no>
2022-12-01 16:18:43 +0100

    fixing small error in url strings

--- R/functions/chelsa_trace21k_download.R
+++ R/functions/chelsa_trace21k_download.R
@@ -8,11 +8,12 @@ get_chelsa_trace21k_urls <- function(variables = c("bio", "tasmin", "tasmax", "p
                                      month.var = c(1:12),
                                      time.var = c(20:-220)) {
   
-  base_url <- "https://envidatrepo.wsl.ch/uploads/chelsa/chelsa_V1/chelsa_trace/"
+  base_url <- "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/"
   
   expand_grid(model = name, time.id = time.var, variable = variables,  bio = bio.var, month = month.var) %>%
     mutate(
-      month = case_when(variable == "bio" ~ month == NA),
+      month = replace(month, variable == "bio", NA),
+      bio = replace(bio, variable != "bio", NA),
       histdir = case_when(variable == "bio" ~ "bio/",
                         variable == "tasmin" ~ "tasmin/",
                         variable == "tasmax" ~ "tasmax/",
@@ -20,9 +21,86 @@ get_chelsa_trace21k_urls <- function(variables = c("bio", "tasmin", "tasmax", "p
       file = case_when(variable == "bio" ~ 
                      paste0(histdir, model, "_", variable, str_pad(bio, 2, "left", "0"), "_", time.id,"_V1.0.tif"),
                    variable != "bio" ~
-                     paste0(histdir, model, "_", variable, str_pad(month, 2, "left", "0"), "_", time.id, "_V1.0.tif")),
+                     paste0(histdir, model, "_", variable, "_", month, "_", time.id, "_V1.0.tif")),
   url = paste0(base_url, file)) %>%
     dplyr::select(file, url) %>%
     distinct()
 }
+# clipping = FALSE,
+# clip.shapefile = NULL,
+# buffer = 0,
+# clip.extent = c(-180, 180, -90, 90),
+# convert.files.to.asc = FALSE,
+# stacking.data = FALSE,
+# combine.raw.zip = FALSE,
+# delete.raw.data = TRUE,
+# save.bib.file = FALSE
+
+
+
+
+
+test <- get_chelsa_trace21k_urls(variables = "tasmin",
+                         bio.var = c(1,5),
+                         month.var = 10,
+                         time.var = c(20:15)) %>%
+  ch_dl
+
+
+
+
+ch_dl <- function(md, dest=NULL, skip_existing=TRUE, method="curl", crop=NULL){
+  
+  if(is.null(dest)) dest <- getwd()
+  
+  for(i in 1:nrow(md)){
+    message(paste("File", i, "of", nrow(md), "..."))
+    md$status[i] <- "incomplete"
+    md$path[i] <- paste0(dest, "/", basename(md$file[i]))
+    
+    runs <- c("1", "2", "12")
+    
+    if(skip_existing){
+      # previously-failed downloads have small file size
+      paths <- sapply(runs, function(x) sub("\\*", x, md$path[i]))
+      size <- file.size(paths)
+      if(any(!is.na(size) & log(size)>10)){
+        md$path[i] <- paths[!is.na(size) & log(size)>10]
+        md$status[i] <- "already done"
+        next()
+      }
+    }
+    
+    # run numbers vary by model. try all options.
+    for(run in runs){
+      url <- sub("\\*", run, md$url[i])
+      path <- sub("\\*", run, md$path[i])
+      r <- try(download.file(url, path, method=method, quiet=T))
+      size <- file.size(path)
+      if(!is.na(size) & log(size)>10){
+        md$url[i] <- url
+        md$path[i] <- path
+        break()
+      }
+      file.remove(path)
+    }
+    
+    if(class(r)=="try-error"){
+      md$status[i] <- as.character(r)
+      next()
+    }
+    if(file.exists(md$path[i])) md$status[i] <- "download completed"
+    
+    if(!is.null(crop) & file.exists(md$path[i])){
+      require(raster)
+      r <- raster(md$path[i]) %>%
+        crop(crop) %>%
+        writeRaster(md$path[i], overwrite=T)
+      md$status[i] <- "raster cropped"
+    }
+  }
+  return(md)
+}
+
+
 
