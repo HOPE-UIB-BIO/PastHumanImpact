4444e342caeeee08bb2ebf45d631023d3aa3d171
Vivian_A_F <vivian.felde@uib.no>
2022-12-01 16:33:29 +0100

    start modify function

--- R/functions/chelsa_trace21k_download.R
+++ R/functions/chelsa_trace21k_download.R
@@ -26,27 +26,6 @@ get_chelsa_trace21k_urls <- function(variables = c("bio", "tasmin", "tasmax", "p
     dplyr::select(file, url) %>%
     distinct()
 }
-# clipping = FALSE,
-# clip.shapefile = NULL,
-# buffer = 0,
-# clip.extent = c(-180, 180, -90, 90),
-# convert.files.to.asc = FALSE,
-# stacking.data = FALSE,
-# combine.raw.zip = FALSE,
-# delete.raw.data = TRUE,
-# save.bib.file = FALSE
-
-
-
-
-
-test <- get_chelsa_trace21k_urls(variables = "tasmin",
-                         bio.var = c(1,5),
-                         month.var = 10,
-                         time.var = c(20:15)) %>%
-  ch_dl
-
-
 
 
 ch_dl <- function(md, dest=NULL, skip_existing=TRUE, method="curl", crop=NULL){
@@ -101,6 +80,84 @@ ch_dl <- function(md, dest=NULL, skip_existing=TRUE, method="curl", crop=NULL){
   }
   return(md)
 }
+
+################### TEST
+test <- get_chelsa_trace21k_urls(variables = "tasmin",
+                         bio.var = c(1,5),
+                         month.var = 10,
+                         time.var = c(20:15)) %>%
+  ch_dl
+
+#------------- ok
+
+
+
+################# MODIFY
+# modify code so that for each iteration: import data to R, extract data into lists and remove downloaded file to avoid filling up storage 
+
+
+ch_dl_version2 <- function(md, dest=NULL, skip_existing=TRUE, method="curl", crop=NULL){
+  
+  if(is.null(dest)) dest <- getwd()
+  
+  for(i in 1:nrow(md)){
+    message(paste("File", i, "of", nrow(md), "..."))
+    md$status[i] <- "incomplete"
+    md$path[i] <- paste0(dest, "/", basename(md$file[i]))
+    
+    runs <- c("1", "2", "12")
+    
+    if(skip_existing){
+      # previously-failed downloads have small file size
+      paths <- sapply(runs, function(x) sub("\\*", x, md$path[i]))
+      size <- file.size(paths)
+      if(any(!is.na(size) & log(size)>10)){
+        md$path[i] <- paths[!is.na(size) & log(size)>10]
+        md$status[i] <- "already done"
+        next()
+      }
+    }
+    
+    # run numbers vary by model. try all options.
+    for(run in runs){
+      url <- sub("\\*", run, md$url[i])
+      path <- sub("\\*", run, md$path[i])
+      r <- try(download.file(url, path, method=method, quiet=T))
+      size <- file.size(path)
+      if(!is.na(size) & log(size)>10){
+        md$url[i] <- url
+        md$path[i] <- path
+        break()
+      }
+      file.remove(path)
+    }
+    
+    if(class(r)=="try-error"){
+      md$status[i] <- as.character(r)
+      next()
+    }
+    if(file.exists(md$path[i])) md$status[i] <- "download completed"
+    
+    if(!is.null(crop) & file.exists(md$path[i])){
+      require(raster)
+      r <- raster(md$path[i]) %>%
+        crop(crop) %>%
+        writeRaster(md$path[i], overwrite=T)
+      md$status[i] <- "raster cropped"
+    }
+    
+   # IMPORT/SAVE DATA TO R 
+   # CREATE TABLE
+   # REMOVE/DELETE DOWNLOADED FILE 
+    
+    
+    
+  }
+  
+  # STACK TIME SERIES/GRIDDED DATA AND RETURN TIBBLE 
+  return(md)
+}
+
 
 
 
