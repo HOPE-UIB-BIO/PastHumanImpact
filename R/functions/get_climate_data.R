2119d879bfd544f7371522ef46dcdccfd746762e
Vivian_A_F <vivian.felde@uib.no>
2022-12-03 20:46:44 +0100

    new wrapper function to download HOPE climate data

--- R/functions/get_climate_data.R
+++ R/functions/get_climate_data.R
@@ -1,40 +1,61 @@
+#' @title A wrapper function to download palaeoclimatic variables from CHELSA 
+#' @description This is wrapper function that create a tibble with selected variables to be downloaded, download, extract relevant data and delete the downloaded .tif files. 
+#' @return Provide the selected climatic data for each location and a meta data table with the file and url links
+
+#ISSUES
+# can we speed download up in parallel sessions?
+# cores <- parallel::detectCores()-1
+# plan(multisession, workers = cores)
+
+# how to save data stepwise if we need to break download?
+
+
+get_climate_data <- function(variables.selected = c("bio", "tasmin"),
+                             bio.var.selected = c(1, 6, 12, 15, 18, 19),
+                             time.var.selected = c(20:-200),
+                             month.var.selected = c(1:12),
+                             xy = data_meta) {
+  
+  
+  # meta data into data.frame with x (long) y (lat) data only
+  xy_data <- xy %>% 
+    dplyr::select(dataset_id, long, lat) %>% 
+    column_to_rownames("dataset_id")
+  
+  # select variables and download CHELSA data
+  climate_dl <- get_chelsa_trace21k_urls(variables = variables.selected, 
+                                           bio.var = bio.var.selected,
+                                           month.var = month.var.selected,
+                                           time.var = time.var.selected) %>%  
+    get_chelsa_download(., extract.data = xy_data) 
+  
+ # restructure downloaded climate data
+  climate_tables <-  climate_dl %>% 
+    dplyr::select(variable, time.id, bio, month, climate) %>%
+    unnest(cols = climate) %>%
+    dplyr::select(variable:month, dataset_id, value) %>%
+    mutate(variable = case_when(
+      variable == "bio" ~ paste0(variable, bio),
+      TRUE ~ paste0(variable, month))) %>%
+    dplyr::select(-c(bio, month)) %>%
+    nest(climate = -dataset_id) %>%
+    mutate(climate = purrr::map(climate, function(.x) {.x %>% arrange(variable)}))
+  
+  
+  # table of meta data
+  climate_meta <- climate_dl %>% 
+    dplyr::select(-c(histdir, path, climate))
+  
+  climate_data <- list(data = climate_tables, meta = climate_meta)
+  
+  return(climate_data)
+}
+
+
+  
 
 
 
 
 
-get_climate_data <- 
-  function(
-    data_source,
-    sel_type = "bio01",
-    var_name,
-    path_vector,
-    path_to_save){
-    
-    if(missing(var_name)){
-      var_name <- sel_type
-    }
-    
-    if(missing(path_to_save)){
-      path_to_save <- here::here("Outputs/Data", var_name)
-    }
-    
-    clim_data <- 
-      load_climate_data(sel_type = sel_type,
-                path_vector)
-    
-    data_ext <-
-      extract_climate_data(
-        clim_data_raster_list = clim_data,
-        data_source = data_source,
-        sel_type = sel_type) 
-    
-    var_name_dat <- paste0(var_name, "_data")
-    
-    data_w <- 
-      data_ext %>% 
-      dplyr::rename(
-        !!var_name_dat := var)
-    
-    return(data_w)
-  }
+
