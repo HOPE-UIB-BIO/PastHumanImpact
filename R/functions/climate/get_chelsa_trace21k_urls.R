f8876bb6ab5ed9125baacf0dec587f3407825a9b
Vivian-Felde <vivian.felde@uib.no>
2022-12-05 16:55:12 +0100

    fix parameters in climate function (#39)

--- R/functions/climate/get_chelsa_trace21k_urls.R
+++ R/functions/climate/get_chelsa_trace21k_urls.R
@@ -3,13 +3,13 @@
 #' @return A tibble to be used with the_get chelsa_download function
 get_chelsa_trace21k_urls <- function(variables = c("bio", "tasmin", "tasmax", "pr"),
                                      name = "CHELSA_TraCE21k",
-                                     bio.var = c(1:19),
-                                     month.var = c(1:12),
-                                     time.var = c(20:-220)) {
+                                     bio_var = c(1:19),
+                                     month_var = c(1:12),
+                                     time_var = c(20:-220)) {
   
   base_url <- "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_trace/"
   
-  expand_grid(model = name, time.id = time.var, variable = variables,  bio = bio.var, month = month.var) %>%
+  expand_grid(model = name, time_id = time_var, variable = variables,  bio = bio_var, month = month_var) %>%
     mutate(
       month = replace(month, variable == "bio", NA),
       bio = replace(bio, variable != "bio", NA),
@@ -18,9 +18,9 @@ get_chelsa_trace21k_urls <- function(variables = c("bio", "tasmin", "tasmax", "p
                           variable == "tasmax" ~ "tasmax/",
                           variable == "pr"~ "pr/"),
       file = case_when(variable == "bio" ~ 
-                         paste0(histdir, model, "_", variable, str_pad(bio, 2, "left", "0"), "_", time.id,"_V1.0.tif"),
+                         paste0(histdir, model, "_", variable, str_pad(bio, 2, "left", "0"), "_", time_id,"_V1.0.tif"),
                        variable != "bio" ~
-                         paste0(histdir, model, "_", variable, "_", month, "_", time.id, "_V1.0.tif")),
+                         paste0(histdir, model, "_", variable, "_", month, "_", time_id, "_V1.0.tif")),
       url = paste0(base_url, file)) %>%
     #dplyr::select(file, url) %>%
     distinct()
