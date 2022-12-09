0b3e3756cf29bc1648da316c8ce7bd0d01f89efa
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-12-09 16:38:18 +0100

    lintr

--- R/functions/hvarpart/perm_hvarpart.R
+++ R/functions/hvarpart/perm_hvarpart.R
@@ -1,107 +1,106 @@
 #' @title Permutation of hierarchial variation partitioning
-#' @description This function permute the hierarchial variation partitioning, either random or a time series design with mirror reflection
+#' @description This function permute the hierarchial variation partitioning,
+#' either random or a time series design with mirror reflection
 #' @param dv response data frame
-#' @param iv either a data frame of predictor variables or list of data frames of predictor variables
+#' @param iv either a data frame of predictor variables or list of data frames
+#' of predictor variables
 #' @param scale logical; standardize to unit variance when RDA method is applied
 #' @param add logical; if constant should be added using db-RDA method
 #' @param sqrt.dist logical; if square-root of dissimilarity should be added
-#' @param nperm integer; number of permutation for adj R squared using CCA method
+#' @param nperm integer; number of permutation for adj R squared using CCA
+#' method
 #' @param permutations integer; number of permutation for randomised datasets
 #' @return provide p-values of varpart
 
+perm_hvarpart <- function(dv,
+                          iv,
+                          method = c("RDA", "dbRDA", "CCA"),
+                          type = c("adjR2", "R2"),
+                          scale = FALSE,
+                          add = FALSE, sqrt.dist = FALSE,
+                          n.perm = 1000,
+                          permutations = 1000,
+                          series = TRUE) {
+  obs <- rdacca.hp(dv, iv,
+    method = method, type = type, scale = scale,
+    add = add, sqrt.dist = sqrt.dist, n.perm = n.perm
+  )
 
-
-perm_hvarpart <- function(dv, 
-                         iv, 
-                         method = c("RDA", "dbRDA", "CCA"), 
-                         type = c("adjR2", "R2"), 
-                         scale = FALSE, 
-                         add = FALSE, sqrt.dist = FALSE, 
-                         n.perm = 1000, 
-                         permutations = 1000,
-                         series = TRUE) {
-  
-  
-  obs <- rdacca.hp(dv, iv, method = method, type = type, scale = scale, 
-                   add = add, sqrt.dist = sqrt.dist, n.perm = n.perm)
-  
   r2q <- obs$Hier.part[, 3]
-  
-  if(series == TRUE & is.data.frame(iv)) {
-    
+
+  if (series == TRUE & is.data.frame(iv)) {
     n <- dim(iv)[1]
-    nvar <- dim(iv)[2]    
-    
+    nvar <- dim(iv)[2]
+
     # permute design for time series
     ctrl <- permute::how(within = Within(type = "series", mirror = TRUE))
     perms <- allPerms(seq_len(n), control = ctrl)
     permutations <- ncol(perms)
-    
-    cat("\nPlease wait: running", permutations, "permutations \n") 
-    
+
+    cat("\nPlease wait: running", permutations, "permutations \n")
+
     for (i in 1:permutations) {
       newiv <- iv
       for (j in 1:nvar) {
-        newiv[, j] <- iv[perms[i,], j]
+        newiv[, j] <- iv[perms[i, ], j]
       }
-      
-      simu = rdacca.hp(dv, newiv, method = method, type = type, 
-                       scale = scale, add = add, sqrt.dist = sqrt.dist, 
-                       n.perm = n.perm)
-      
-      r2q = cbind(r2q, simu$Hier.part[, 3])
+
+      simu <- rdacca.hp(dv, newiv,
+        method = method, type = type,
+        scale = scale, add = add, sqrt.dist = sqrt.dist,
+        n.perm = n.perm
+      )
+
+      r2q <- cbind(r2q, simu$Hier.part[, 3])
     }
-  }
-  else if(series == FALSE & is.data.frame(iv)) {
-    
-    cat("\nPlease wait: running", permutations - 1, "permutations \n")  
-    
+  } else if (series == FALSE & is.data.frame(iv)) {
+    cat("\nPlease wait: running", permutations - 1, "permutations \n")
+
     for (i in 1:permutations - 1) {
       newiv <- iv
       for (j in 1:nvar) {
         perms <- sample(1:n, n)
         newiv[, j] <- iv[, j][perms]
       }
-      
+
       row.names(newiv) <- 1:n
-      simu = rdacca.hp(dv, newiv, method = method, type = type, 
-                       scale = scale, add = add, sqrt.dist = sqrt.dist, 
-                       n.perm = n.perm)
-      r2q = cbind(r2q, simu$Hier.part[, 3])
+      simu <- rdacca.hp(dv, newiv,
+        method = method, type = type,
+        scale = scale, add = add, sqrt.dist = sqrt.dist,
+        n.perm = n.perm
+      )
+      r2q <- cbind(r2q, simu$Hier.part[, 3])
     }
-  }
-  else if(series == TRUE & !is.data.frame(iv)){
-    
-    
-    
+  } else if (series == TRUE & !is.data.frame(iv)) {
     n <- dim(iv[[1]])[1]
     nvar <- length(iv)
-    
+
     ctrl <- permute::how(within = Within(type = "series", mirror = TRUE))
     perms <- allPerms(seq_len(n), control = ctrl)
     permutations <- ncol(perms)
-    
+
     cat("\nPlease wait: running", permutations, "permutations \n")
-    
+
     for (i in 1:permutations) {
       newiv <- list()
       for (j in 1:nvar) {
-        newiv[[j]] <- iv[perms[i,],]
+        newiv[[j]] <- iv[perms[i, ], ]
       }
-      
+
       names(newiv) <- names(iv)
-      simu = rdacca.hp(dv, newiv, method = method, type = type, 
-                       scale = scale, add = add, sqrt.dist = sqrt.dist, 
-                       n.perm = n.perm)
-      r2q = cbind(r2q, simu$Hier.part[, 3])
+      simu <- rdacca.hp(dv, newiv,
+        method = method, type = type,
+        scale = scale, add = add, sqrt.dist = sqrt.dist,
+        n.perm = n.perm
+      )
+      r2q <- cbind(r2q, simu$Hier.part[, 3])
     }
   } else {
-    
-    cat("\nPlease wait: running", permutations-1, "permutations \n") 
-    
+    cat("\nPlease wait: running", permutations - 1, "permutations \n")
+
     n <- dim(iv[[1]])[1]
     nvar <- length(iv)
-    
+
     for (i in 1:permutations - 1) {
       perms <- sample(1:n, n)
       newiv <- list()
@@ -110,16 +109,20 @@ perm_hvarpart <- function(dv,
         row.names(newiv[[j]]) <- 1:n
       }
       names(newiv) <- names(iv)
-      simu = rdacca.hp(dv, newiv, method = method, type = type, 
-                       scale = scale, add = add, sqrt.dist = sqrt.dist, 
-                       n.perm = n.perm)
-      r2q = cbind(r2q, simu$Hier.part[, 3])
+      simu <- rdacca.hp(dv, newiv,
+        method = method, type = type,
+        scale = scale, add = add, sqrt.dist = sqrt.dist,
+        n.perm = n.perm
+      )
+      r2q <- cbind(r2q, simu$Hier.part[, 3])
     }
   }
-  
-  Signi = function(x) {
-    pval = round(1 - ecdf(x)(x[1]) + 1/(permutations + 1), 
-                 nchar(permutations))
+
+  Signi <- function(x) {
+    pval <- round(
+      1 - ecdf(x)(x[1]) + 1 / (permutations + 1),
+      nchar(permutations)
+    )
     if (pval <= 0.001) {
       return(noquote(paste(pval, "***", sep = " ")))
     }
@@ -128,14 +131,13 @@ perm_hvarpart <- function(dv,
     }
     if (pval <= 0.05) {
       return(noquote(paste(pval, "  *", sep = " ")))
-    }
-    else {
+    } else {
       return(noquote(paste(pval, "   ", sep = " ")))
     }
   }
-  
-  p.R2 = apply(r2q, 1, Signi)
-  result = data.frame(obs$Hier.part[, 3], Pr = p.R2)
+
+  p.R2 <- apply(r2q, 1, Signi)
+  result <- data.frame(obs$Hier.part[, 3], Pr = p.R2)
   colnames(result) <- c("Individual", "Pr(>I)")
   return(result)
 }
