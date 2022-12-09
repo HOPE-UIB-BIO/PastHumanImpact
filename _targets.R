ed18c84a36067a79baa9279f09232c3c7f0ecbb9
Vivian_A_F <vivian.felde@uib.no>
2022-12-09 10:48:10 +0100

    fixed and added code in _targets

--- _targets.R
+++ _targets.R
@@ -447,8 +447,8 @@ list(
   ),
   # - combine all PAP estimates into one tibble
   targets::tar_target(
-    name = data_combined_paps,
-    command = combine_pap(
+    name = data_prepared_cp,
+    command = prepare_data_cp(
       data_pollen,
       data_diversity,
       data_mrt,
@@ -459,7 +459,14 @@ list(
   # - calculate change points of all PAP variables by regression trees (RT)
   targets::tar_target(
     name = data_change_points,
-    command = get_change_points_pap(data_combined_paps)
+    command = get_change_points(data_prepared_cp)
+  ),
+  # - calculate density of change points
+  targets::tar_target(
+    name = data_density,
+    command = get_density_pap(datasource = data_change_points,
+                              meta_data_source = data_meta,
+                              dummy_table_source = ) 
   )
 )
 
@@ -467,9 +474,8 @@ list(
 
 
 # DENSITY ESTIMATION:
-# EVEN SPACING; ERROR PROPAGATION; HGAM DENSITY COMBINED VARS
+# EVEN SPACING; HGAM DENSITY COMBINED VARS
 
-# tar_target(data_density, get_density_pap(data_change_points_pap))
 
 # H1 targets
 # tar_target(data_h1, get_data_h1(data_meta, data_combined_paps, data_density, data_events, data_climate, data_spd))
