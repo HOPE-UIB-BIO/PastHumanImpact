0d93e2158f68b14fc3e5119dfbd9454992da1320
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2022-12-12 10:49:44 +0100

    hierarchical VAR-PAR - list-based groups (#48)

--- R/functions/hvarpart/run_hvarpart.R
+++ R/functions/hvarpart/run_hvarpart.R
@@ -1,29 +1,55 @@
 #' @title Run hierarchial variation partitioning
-#' @description This wrapper run the hierarchical variation partitioning for the HOPE dataset
-#' @param datasource input tibble 
-#' @param perm integer; number of permutations for p-values
-#' @param reponse_var vector of variables to be included in the response dataset
-#' @param predictor_vars vector of variables to be included in the predictor dataset
-#' @param choose_matrix logical; if predictor variables should be assessed individual or as list of data frames
-#' @return List of model outputs and a summary table of the results 
+#' @description This wrapper run the hierarchical variation
+#' partitioning for the HOPE dataset
+#' @param data_source input tibble
+#' @param reponse_vars
+#' vector of variables to be included in the response datasetet
+#' @param predictor_vars
+#' vector of names of predictor variables or list with names
+#' @param run_all_predictors logical; if predictor variables should be assessed
+#' individual or as list of data frames
+#' @param time_series logical; Should permutation be used for ordered?
+#' @param get_significance logical; Should significance of predictors be
+#' estimated? (takes along time)
+#' @param permutations integer; number of permutations for p-values
 
+run_hvarpart <- function(data_source,
+                         response_vars = c(
+                           "n0", "n1", "n2",
+                           "n1_minus_n2", "n2_divided_by_n1", "n1_divided_by_n0",
+                           "roc",
+                           "dcca_axis_1"
+                         ),
+                         predictor_vars = list(
+                           human = c("spd"),
+                           climate = c(
+                             "temp_cold",
+                             "prec_summer",
+                             "prec_win",
+                             "gdm"
+                           ),
+                           time = c("age")
+                         ),
+                         run_all_predictors = FALSE,
+                         time_series = TRUE,
+                         get_significance = TRUE,
+                         permutations = 99) {
+  res <-
+    data_source %>%
+    dplyr::mutate(
+      varhp = purrr::map(
+        .x = data_merge,
+        .f = ~ get_varhp(
+          data_source = .x,
+          permutations = permutations,
+          response_vars = response_vars,
+          predictor_vars = predictor_vars,
+          run_all_predictors = run_all_predictors,
+          time_series = time_series,
+          get_significance = get_significance
+        )
+      )
+    )
 
-run_hvarpart <- function(datasource,
-                   perm = 99,
-                   response_var = NULL,
-                   predictor_vars = NULL,
-                   choose_matrix = FALSE,
-                   time_series = TRUE) {
-  
-  res <- datasource %>% 
-    mutate(varhp = purrr::map(data_merge, 
-                            ~get_varhp(., 
-                                       permutations = perm,   
-                                       resp_vars = response_vars, 
-                                       pred_vars = predictor_vars,
-                                       run_matrix = choose_matrix,
-                                       series = time_series))) 
-  
   return(res)
-  
-  }
+}
