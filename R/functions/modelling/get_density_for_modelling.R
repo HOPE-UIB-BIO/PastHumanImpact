ab21b77f1c526878b11c16c56f036749da7fbb85
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-12-12 11:47:22 +0100

    lintr

--- R/functions/modelling/get_density_for_modelling.R
+++ R/functions/modelling/get_density_for_modelling.R
@@ -1,40 +1,43 @@
 #' @title Get density variables for modelling
-#' @description This function creates two combined density variables to be used for hierarchical gam modeling
-#' @param data_source_density the individual density estimates of pap change points
-#' @param data_rescaled Logical. Should the density estimate rescaled to 0-1 be used?
-#' @param select_vars Character vector. Select the variables in varname to be combined
-#' @return Provide common trend of the two combined density variables of change points in diversity and composition
-#' 
+#' @description
+#' This function creates two combined density variables to be used
+#' for hierarchical gam modeling
+#' @param data_source_density
+#' the individual density estimates of pap change points
+#' @param data_rescaled
+#' Logical. Should the density estimate rescaled to 0-1 be used?
+#' @param select_vars
+#' Character vector. Select the variables in varname to be combined
+#' @return Provide common trend of the two combined density variables of
+#' change points in diversity and composition
+#'
 
 get_density_for_modelling <- function(data_source_density,
                                       data_rescaled = TRUE,
                                       select_vars = NULL) {
-  
-  if(data_rescale == TRUE) {
+  if (data_rescale == TRUE) {
     data_source_density %>%
       dplyr::select(dataset_id, pap_density_rescale) %>%
       unnest(pap_density_rescale) %>%
-      pivot_longer(mvrt:n2_divided_by_n1, 
-                   names_to = "var_name", 
-                   values_to = "value") %>%
+      pivot_longer(mvrt:n2_divided_by_n1,
+        names_to = "var_name",
+        values_to = "value"
+      ) %>%
       dplyr::filter(var_name %in% select_vars) %>%
       tidyr::drop_na(value) %>%
       tidyr::nest(data_to_fit = c(dataset_id, age, value)) %>%
       return()
-   
-      
   } else {
     data_source_density %>%
       dplyr::select(dataset_id, pap_density) %>%
       unnest(pap_density) %>%
-      pivot_longer(mvrt:n2_divided_by_n1, 
-                   names_to = "var_name", 
-                   values_to = "value") %>%
+      pivot_longer(mvrt:n2_divided_by_n1,
+        names_to = "var_name",
+        values_to = "value"
+      ) %>%
       dplyr::filter(var_name %in% select_vars) %>%
       tidyr::drop_na(value) %>%
       tidyr::nest(data_to_fit = c(dataset_id, age, value)) %>%
       return()
-    
   }
 }
-  
