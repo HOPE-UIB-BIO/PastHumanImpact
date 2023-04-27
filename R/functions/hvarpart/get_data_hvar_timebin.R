b7b346127c2cf09bcda1aed4819148c186340610
Vivian_A_F <vivian.felde@uib.no>
2023-04-27 10:24:48 +0200

    structure data for time bin analysis and add an extra variable of numbers of samples per time bin

--- R/functions/hvarpart/get_data_hvar_timebin.R
+++ R/functions/hvarpart/get_data_hvar_timebin.R
@@ -8,6 +8,10 @@ get_data_hvar_timebin <- function(data_source,
                 dplyr::select(dataset_id, lat, long, region, ecozone_koppen_5),
               by = "dataset_id") %>%
     nest(data_merge = -c(region, ecozone_koppen_5, age)) %>%
+    mutate(n_samples = purrr::map_dbl(data_merge, 
+                                      .f = function(x){ 
+                                        nrow(x) %>% 
+                                          return()})) %>%
     return()
  
 }
