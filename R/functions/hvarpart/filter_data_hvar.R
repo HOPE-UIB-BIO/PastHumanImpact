086c28a144ff52fd4a8d74d1a5a361a4a9f20110
Vivian Astrup Felde <vivian.felde@uib.no>
2023-06-13 15:16:25 +0200

    fix age table end of period

--- R/functions/hvarpart/filter_data_hvar.R
+++ R/functions/hvarpart/filter_data_hvar.R
@@ -5,12 +5,12 @@ filter_data_hvar <- function(data_source,
     unnest(data_merge) %>%
     drop_na() %>%
     left_join(data_meta %>% 
-                dplyr::select(dataset_id, end_of_interest_period, region),
+                dplyr::select(dataset_id, region),
               by = "dataset_id") %>%
     left_join(age_table, by = "region") %>%
-    filter(age >= age_from & age <= end_of_interest_period) %>%
+    filter(age >= age_from & age <= age_end) %>%
     filter(!region == "Africa") %>%
-    dplyr::select(-c(age_from, end_of_interest_period, region)) %>%
+    dplyr::select(-c(age_from, age_end, region)) %>%
     nest(data_merge = -c(dataset_id)) %>%
     return()
   
