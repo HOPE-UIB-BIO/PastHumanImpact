34aebd2f0f713f2d9866723721a35ae58633208f
Vivian Astrup Felde <vivian.felde@uib.no>
2023-06-28 19:01:32 +0200

    remove aggregate of ecozones

--- R/functions/hvarpart/get_data_hvar_timebin.R
+++ R/functions/hvarpart/get_data_hvar_timebin.R
@@ -5,9 +5,9 @@ get_data_hvar_timebin <- function(data_source,
     unnest(data_merge) %>%
     drop_na() %>%
     left_join(data_meta %>% 
-                dplyr::select(dataset_id, lat, long, region, ecozone_koppen_5),
+                dplyr::select(dataset_id, lat, long, region),
               by = "dataset_id") %>%
-    nest(data_merge = -c(region, ecozone_koppen_5, age)) %>%
+    nest(data_merge = -c(region, age)) %>%
     mutate(n_samples = purrr::map_dbl(data_merge, 
                                       .f = function(x){ 
                                         nrow(x) %>% 
