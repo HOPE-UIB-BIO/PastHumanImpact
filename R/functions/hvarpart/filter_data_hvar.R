de62882585334338c259a0fa7992d15833621bd3
Vivian Astrup Felde <vivian.felde@uib.no>
2023-07-10 11:27:53 +0200

    filter out data not to be included

--- R/functions/hvarpart/filter_data_hvar.R
+++ R/functions/hvarpart/filter_data_hvar.R
@@ -5,12 +5,13 @@ filter_data_hvar <- function(data_source,
     unnest(data_merge) %>%
     drop_na() %>%
     left_join(data_meta %>% 
-                dplyr::select(dataset_id, region),
+                dplyr::select(dataset_id, region, data_publicity),
               by = "dataset_id") %>%
     left_join(age_table, by = "region") %>%
-    filter(age >= age_from & age <= age_end) %>%
+    dplyr::filter(!(region == "Latin America" & data_publicity == "private")) %>%
     filter(!region == "Africa") %>%
-    dplyr::select(-c(age_from, age_end, region)) %>%
+    filter(age >= age_from & age <= age_end) %>%
+    dplyr::select(-c(age_from, age_end, region, data_publicity)) %>%
     nest(data_merge = -c(dataset_id)) %>%
     return()
   
