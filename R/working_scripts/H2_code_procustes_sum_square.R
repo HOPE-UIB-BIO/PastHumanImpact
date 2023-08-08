1f3af61756192f2e517c374d2d6ca0a9366b5cf1
Vivian-Felde <vivian.felde@uib.no>
2023-08-08 16:07:01 +0200

    H2: Update legacy code to get procrustes sum of squares (#89)

---
+++ R/working_scripts/H2_code_procustes_sum_square.R
@@ -0,0 +1,184 @@
+# test hypothesis 2 interrelationship in time
+
+
+data_to_run <- targets::tar_read(data_hvar_filtered)
+data_meta <- targets::tar_read(data_meta)
+
+
+select_vars <- c("dataset_id","age", "n0", "n1", "n2", "n1_minus_n2", "n2_divided_by_n1" , "n1_divided_by_n0",  "dcca_axis_1", "roc", "density_turnover",  "density_diversity")
+
+
+# Prepare data - filter out timesteps with less than 3 samples
+data_for_h2 <- 
+  data_to_run %>% 
+  unnest(data_merge) %>% 
+  dplyr::select(all_of(select_vars)) %>%
+  left_join(data_meta %>% 
+              dplyr::select(dataset_id, lat, long, region, ecozone_koppen_5),
+            by = "dataset_id") %>%
+  drop_na() %>%
+  nest(data = -c("age", "ecozone_koppen_5", "region")) %>%
+  dplyr::mutate(n_samples = purrr::map_dbl(data, ~nrow(.x))) %>%
+  dplyr::filter(n_samples > 3)
+
+
+# FUNCTIONS
+# function to get procrustes
+get_procrustes_m2 <- function(data_list) {
+  
+  name_vec <- names(data_list)
+  
+  len <- length(data_list)
+  
+  procrustes_m2 <- NULL
+  for (i in 1:len){
+    prot2 <- NULL
+    for (j in 1:len) {
+      prot1 <- ifelse(j > i, NA, vegan::procrustes(X = data_list[[i]], 
+                                                   Y = data_list[[j]], 
+                                                   scale = TRUE, 
+                                                   symmetric = TRUE, 
+                                                   scores = "species")$ss)
+      prot2 <- c(prot2, prot1)
+    }
+    procrustes_m2 <- rbind(procrustes_m2, prot2)
+  }
+  
+  rownames(procrustes_m2) <- name_vec
+  colnames(procrustes_m2) <- name_vec
+  
+  return(procrustes_m2)
+  
+}
+
+
+# wrapper to run pca
+run_pca <- function(x, scale = TRUE){
+  resp <- x %>% 
+    dplyr::select(n0:density_diversity)
+  mod <- vegan::rda(resp, 
+                    scale = scale, 
+                    data = x)
+  return(mod)
+}
+
+# get pcoa scores
+get_scores <- function(pcoa, region, ecozone) {
+  scores_df <- data.frame(pcoa$points) %>%
+    rownames_to_column("label") %>% 
+    mutate(region = region) %>%
+    mutate(ecozone_koppen_5 = ecozone)
+  scores_df
+}
+
+# get m2 difference order by time
+get_m2_time <- function(data) {
+  vec <- data[-1,] %>% diag()
+  names(vec) <- rownames(data[-1,])
+  vec
+}
+
+get_m2_time_df <- function(data) {
+  df <- data %>% 
+    data.frame(delta_m2 = .)%>%
+    rownames_to_column("time")
+  df
+}
+
+
+# TEST RUN DATA
+# Run PCA analyses for each time bin in regional ecozones; get procrustes sum of square, extract difference with time
+pap_procrustes_ecozones <- data_for_h2 %>% 
+  mutate(pca_analysis = purrr::map(data,
+                                   .f = run_pca)) %>%
+  mutate(pca_analysis = pca_analysis %>% 
+           rlang::set_names(nm = data_for_h2$age))  %>% 
+  group_by(region, ecozone_koppen_5) %>%
+  summarise(pca_analysis = list(pca_analysis)) %>% 
+  mutate(m2 = purrr::map(pca_analysis, get_procrustes_m2))%>%
+  ungroup() %>%
+  mutate(m2_time = purrr::map(m2, .f = get_m2_time))
+
+
+
+
+# Add principal coordinate analysis of similarities and differences with time
+pcoa_ecozones <- 
+  pap_procrustes_ecozones %>%
+  filter(!c(region == "North America" & ecozone_koppen_5 == "Tropical")) %>%
+  mutate(PCoA = purrr::map(m2, .f = function(x){
+    procrust.pcoa <- stats::cmdscale(stats::as.dist(x), eig = TRUE, add = TRUE)
+    procrust.pcoa
+  })) %>%
+  mutate(site_scores = purrr::pmap(list(PCoA,
+                                        region,
+                                        ecozone_koppen_5),
+                                   .f = ~get_scores(pcoa = ..1,
+                                                    region = ..2,
+                                                    ecozone = ..3))) %>%
+  mutate(m2_time_df = purrr::map(m2_time, 
+                                 .f = get_m2_time_df))
+
+
+# plot pcoa diagrams 
+pcoa_ecozones %>% 
+  dplyr::select(site_scores) %>% 
+  unnest(cols = c(site_scores)) %>%
+  ggplot(aes(x = X1, y = X2, label = label, col = ecozone_koppen_5)) +
+  coord_fixed() +
+  geom_hline(yintercept = 0, linetype = "dashed", col = "grey", size = 1) +
+  geom_vline(xintercept = 0, linetype = "dashed", col = "grey", size = 1) +
+  geom_point() + 
+  geom_text(hjust = 0, nudge_x = 0.005) +
+  labs(x = "PCoA1", y = "PCoA2") +
+  facet_wrap(~region)+
+  theme_bw() 
+
+
+# PLOT change in m2 in consecutive time steps
+pcoa_ecozones %>%
+  dplyr::select(m2_time_df, region, ecozone_koppen_5) %>%
+  unnest(cols = c(m2_time_df)) %>%
+  ggplot(aes(x = as.numeric(time), y = delta_m2, col = ecozone_koppen_5, fill = ecozone_koppen_5 )) +
+  geom_point() +
+  geom_smooth() +
+  scale_x_reverse() +
+  coord_flip() +
+  facet_wrap(~region)
+
+
+
+## 
+
+# function to get the full procustes analysis in time order?
+# Need this to extract the information about changes in interrelationship variables 
+
+
+get_protest_time <- function(data_list) {
+  
+  name_vec <- names(data_list)
+  
+  protest_list <- list()
+  for (i in 1:length(data_list)) {
+    protest1 <- NULL
+    for (j in 1:length(data_list)) {
+      
+      protest2 <- ifelse(j > i, NA, vegan::protest(X = data_list[[i]], 
+                                                   Y = data_list[[j]], 
+                                                   scale = TRUE, 
+                                                   symmetric = TRUE, 
+                                                   scores = "species",
+                                                   permutation = 499))
+      
+      protest1 <- list(protest1, protest2)
+    }
+    protest_list <- c(protest_list, protest1)
+  }
+  
+  
+  
+  return(protest_list)
+  
+}
+
+get_protest_time(pap_procrustes_ecozones$pca_analysis[[1]])
