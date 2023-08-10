dbb96910104ae734af249b1fb7582e0016521148
OndrejMottl <ondrej.mottl@gmail.com>
2023-08-10 10:54:11 +0200

    h2_target_pipeline: prepare dara for static branching, prepare targets for modeling

--- R/hypothesis_2/h2_target_pipeline.R
+++ R/hypothesis_2/h2_target_pipeline.R
@@ -35,6 +35,52 @@ targets::tar_option_set(
   storage = "worker"
 )
 
+#----------------------------------------------------------#
+# 1. Load and prepare ecozone data -----
+#----------------------------------------------------------#
+
+# This step is important for cretaing a static branching pipeline
+#   AKA making many targets at the same time withoud specifically declering them
+
+data_for_hvar <-
+  targets::tar_read(
+    name = "data_for_hvar",
+    store = paste0(
+      data_storage_path,
+      "_targets_h1"
+    )
+  )
+
+data_meta <-
+  targets::tar_read(
+    name = "data_meta",
+    store = paste0(
+      data_storage_path,
+      "_targets_h1"
+    )
+  )
+
+data_ecozone <-
+  data_meta %>%
+  dplyr::inner_join(
+    data_for_hvar %>%
+      dplyr::distinct(dataset_id),
+    by = "dataset_id"
+  ) %>%
+  dplyr::distinct(
+    region, ecozone_koppen_15, dataset_id
+  )
+
+data_valid_ecozones <-
+  data_ecozone %>%
+  dplyr::group_by(region, ecozone_koppen_15) %>%
+  dplyr::summarise(
+    .groups = "drop",
+    n_datasets = n_distinct(dataset_id)
+  ) %>%
+  dplyr::filter(n_datasets > 5) %>%
+  dplyr::arrange(n_datasets) %>%
+  dplyr::select(-n_datasets)
 
 #----------------------------------------------------------#
 # 1. Targets -----
@@ -42,35 +88,48 @@ targets::tar_option_set(
 
 # the targets list:
 list(
-  # 0. setting variables ----
   targets::tar_target(
-    name = data_dummy_time,
+    name = max_temporal_k,
+    command = 12
+  ),
+  targets::tar_target(
+    name = vec_predictors,
+    command = c(
+      "temp_annual",
+      "temp_cold",
+      "prec_annual",
+      "prec_summer",
+      "prec_win",
+      "spd"
+    )
+  ),
+  targets::tar_target(
+    name = data_pred_errors,
     command = tibble::tibble(
-      age = seq(
-        from = min_age, # [config]
-        to = max_age, # [config]
-        by = timestep # [config]
+      predictor = vec_predictors,
+      error_family = c(
+        rep(
+          "stats::gaussian(link = 'identity')", ,
+          2
+        ),
+        rep(
+          "stats::Gamma(link = 'log')", 3
+        ),
+        "mgcv::tw(link = 'log')"
       )
     )
   ),
   targets::tar_target(
-    name = age_cutoff_region,
-    command = tibble::tibble(
-      region = c(
-        "Europe",
-        "Latin America",
-        "Asia",
-        "Africa",
-        "North America",
-        "Oceania"
-      ),
-      age_from = c(2000, 2000, 2000, 2000, 500, 500),
-      age_end = rep(8500, 6)
+    name = "data_merge",
+    command = dplyr::inner_join(
+      data_ecozone,
+      data_for_hvar
     )
   ),
   targets::tar_target(
-    name = spd_distance_vec,
-    command = c(5, 25, 50, 100, 250, 500) %>%
-      rlang::set_names()
+    name = "data_merge_unnest",
+    command = tidyr::unnest(
+      data_merge, data_merge
+    )
   )
 )
