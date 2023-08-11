9a4ff1b7e9f26821ebd68febc5c0a7fed33fcb9b
OndrejMottl <ondrej.mottl@gmail.com>
2023-08-11 13:10:35 +0200

    get_varhp: lintr

--- R/functions/hvarpart/get_varhp.R
+++ R/functions/hvarpart/get_varhp.R
@@ -36,167 +36,163 @@ get_varhp <- function(data_source,
                       get_significance = TRUE,
                       permutations = 99,
                       ...) {
-  
-  tryCatch({
-  
-  # prepare responses
-  data_resp <-
-    data_source %>%
-    dplyr::select(all_of(response_vars)) %>%
-    dplyr::select(tidyselect:::where(~ any(!is.na(.))))
+  tryCatch(
+    {
+      # prepare responses
+      data_resp <-
+        data_source %>%
+        dplyr::select(all_of(response_vars)) %>%
+        dplyr::select(tidyselect:::where(~ any(!is.na(.))))
 
-  # prepare predictors
-  # if `run_all_predictors` is true then use all variables individually
-  if (
-    isTRUE(run_all_predictors)
-  ) {
-    predictor_vars <-
-      unlist(predictor_vars) %>%
-      rlang::set_names(nm = NULL)
+      # prepare predictors
+      # if `run_all_predictors` is true then use all variables individually
+      if (
+        isTRUE(run_all_predictors)
+      ) {
+        predictor_vars <-
+          unlist(predictor_vars) %>%
+          rlang::set_names(nm = NULL)
 
-    data_preds <-
-      data_source %>%
-      dplyr::select(all_of(predictor_vars)) %>%
-      dplyr::select(tidyselect:::where(~ any(. != 0))) %>%
-      dplyr::select(tidyselect:::where(~ any(!is.na(.))))
-
-    output_table_dummy <-
-      tibble::tibble(
-        predictor = predictor_vars
-      )
-  } else {
-    data_preds <-
-      predictor_vars %>%
-      purrr::map(
-        .x = predictor_vars,
-        .f = ~ data_source %>%
-          dplyr::select(any_of(.x)) %>%
+        data_preds <-
+          data_source %>%
+          dplyr::select(all_of(predictor_vars)) %>%
           dplyr::select(tidyselect:::where(~ any(. != 0))) %>%
           dplyr::select(tidyselect:::where(~ any(!is.na(.))))
-      )
 
-    # filer out groups with no variables
-    data_preds <-
-      data_preds[purrr::map_lgl(
-        data_preds,
-        .f = ~ ncol(.x) > 0
-      )]
+        output_table_dummy <-
+          tibble::tibble(
+            predictor = predictor_vars
+          )
+      } else {
+        data_preds <-
+          predictor_vars %>%
+          purrr::map(
+            .x = predictor_vars,
+            .f = ~ data_source %>%
+              dplyr::select(any_of(.x)) %>%
+              dplyr::select(tidyselect:::where(~ any(. != 0))) %>%
+              dplyr::select(tidyselect:::where(~ any(!is.na(.))))
+          )
 
-    output_table_dummy <-
-      tibble::tibble(
-        predictor = names(predictor_vars)
-      )
-  }
-  
+        # filer out groups with no variables
+        data_preds <-
+          data_preds[purrr::map_lgl(
+            data_preds,
+            .f = ~ ncol(.x) > 0
+          )]
 
-  # run hvarpar
-  # should work for both list and just data.frame
-  varhp <-
-    rdacca.hp::rdacca.hp(
-      dv = vegan::vegdist(data_resp, method = "gower"),
-      iv = data_preds,
-      add = TRUE,
-      method = "dbRDA",
-      type = "adjR2",
-      var.part = TRUE,
-      ...
-    )
+        output_table_dummy <-
+          tibble::tibble(
+            predictor = names(predictor_vars)
+          )
+      }
 
-  # extract relevant summary output
-  output_table <-
-    varhp %>%
-    purrr::pluck("Hier.part") %>%
-    as.data.frame() %>%
-    tibble::rownames_to_column("predictor")
 
-  # test significance
-  if (
-    isTRUE(get_significance)
-  ) {
-    # should work for both list and just data.frame
-    hp_signif <-
-      perm_hvarpart(
-        dv = vegan::vegdist(data_resp, method = "gower"),
-        iv = data_preds,
-        method = "dbRDA",
-        add = TRUE,
-        type = "adjR2",
-        permutations = permutations,
-        series = time_series,
-        verbose = TRUE,
-        ...
-      )
+      # run hvarpar
+      # should work for both list and just data.frame
+      varhp <-
+        rdacca.hp::rdacca.hp(
+          dv = vegan::vegdist(data_resp, method = "gower"),
+          iv = data_preds,
+          add = TRUE,
+          method = "dbRDA",
+          type = "adjR2",
+          var.part = TRUE,
+          ...
+        )
 
-    # extract relevant summary output
-    output_table <-
-      output_table %>%
-      dplyr::left_join(
-        hp_signif,
-        by = "Individual"
-      )
-  }
+      # extract relevant summary output
+      output_table <-
+        varhp %>%
+        purrr::pluck("Hier.part") %>%
+        as.data.frame() %>%
+        tibble::rownames_to_column("predictor")
 
-  # run model to get variation inflation factors of the predictors,
-  #   and total unexplained and explained variation
-  mod <-
-    vegan::capscale(data_resp ~ as.matrix(as.data.frame(data_preds)),
-      dist = "gower",
-      add = TRUE,
-      data_source = data_resp
-    )
+      # test significance
+      if (
+        isTRUE(get_significance)
+      ) {
+        # should work for both list and just data.frame
+        hp_signif <-
+          perm_hvarpart(
+            dv = vegan::vegdist(data_resp, method = "gower"),
+            iv = data_preds,
+            method = "dbRDA",
+            add = TRUE,
+            type = "adjR2",
+            permutations = permutations,
+            series = time_series,
+            verbose = TRUE,
+            ...
+          )
 
-  data_variation <-
-    tibble::tibble(
-      Total_eig = mod %>%
-        purrr::pluck("tot.chi"),
-      Constrained_eig = mod %>%
-        purrr::pluck("CCA") %>%
-        purrr::pluck("eig") %>%
-        sum(),
-      Unconstrained_eig = Total_eig - Constrained_eig
-    )
+        # extract relevant summary output
+        output_table <-
+          output_table %>%
+          dplyr::left_join(
+            hp_signif,
+            by = "Individual"
+          )
+      }
 
-  # add additional VIF information
-  # only works for all predictors
-  if (
-    isTRUE(run_all_predictors)
-  ) {
-    output_table <-
-      output_table %>%
-      dplyr::mutate(
-        Vif = vegan::vif.cca(mod),
-      )
-  }
+      # run model to get variation inflation factors of the predictors,
+      #   and total unexplained and explained variation
+      mod <-
+        vegan::capscale(data_resp ~ as.matrix(as.data.frame(data_preds)),
+          dist = "gower",
+          add = TRUE,
+          data_source = data_resp
+        )
 
-  # left join with all predictors (from `output_table_dummy`)
-  summary_table <-
-    output_table_dummy %>%
-    dplyr::left_join(
-      output_table,
-      by = "predictor"
-    ) %>%
-    # replace all missing values with 0
-    dplyr::mutate(
-      dplyr::across(
-        tidyselect:::where(
-          is.numeric
-        ),
-        ~ tidyr::replace_na(.x, replace = 0)
-      )
-    )
+      data_variation <-
+        tibble::tibble(
+          Total_eig = mod %>%
+            purrr::pluck("tot.chi"),
+          Constrained_eig = mod %>%
+            purrr::pluck("CCA") %>%
+            purrr::pluck("eig") %>%
+            sum(),
+          Unconstrained_eig = Total_eig - Constrained_eig
+        )
 
-  results <-
-    list(
-      varhp_output = varhp,
-      summary_table = summary_table,
-      summary_variation = data_variation
-    )
+      # add additional VIF information
+      # only works for all predictors
+      if (
+        isTRUE(run_all_predictors)
+      ) {
+        output_table <-
+          output_table %>%
+          dplyr::mutate(
+            Vif = vegan::vif.cca(mod),
+          )
+      }
 
-  return(results)
-  
-  },
-  error = function(err) NA
-  
+      # left join with all predictors (from `output_table_dummy`)
+      summary_table <-
+        output_table_dummy %>%
+        dplyr::left_join(
+          output_table,
+          by = "predictor"
+        ) %>%
+        # replace all missing values with 0
+        dplyr::mutate(
+          dplyr::across(
+            tidyselect:::where(
+              is.numeric
+            ),
+            ~ tidyr::replace_na(.x, replace = 0)
+          )
+        )
+
+      results <-
+        list(
+          varhp_output = varhp,
+          summary_table = summary_table,
+          summary_variation = data_variation
+        )
+
+      return(results)
+    },
+    error = function(err) NA
   )
-  
 }
