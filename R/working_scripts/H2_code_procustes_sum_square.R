bdb8440dc9f59e1c219a2ec9f0099bc563f0db43
Vivian Astrup Felde <vivian.felde@uib.no>
2023-08-11 11:23:17 +0200

    update script

--- R/working_scripts/H2_code_procustes_sum_square.R
+++ R/working_scripts/H2_code_procustes_sum_square.R
@@ -1,8 +1,16 @@
 # test hypothesis 2 interrelationship in time
 
 
-data_to_run <- targets::tar_read(data_hvar_filtered)
-data_meta <- targets::tar_read(data_meta)
+data_to_run <- targets::tar_read(data_hvar_filtered,
+                                 store = paste0(
+                                   data_storage_path,
+                                   "_targets_h1"
+                                 ))
+data_meta <- targets::tar_read(data_meta,
+                               store = paste0(
+                                 data_storage_path,
+                                 "_targets_h1"
+                               ))
 
 
 select_vars <- c("dataset_id","age", "n0", "n1", "n2", "n1_minus_n2", "n2_divided_by_n1" , "n1_divided_by_n0",  "dcca_axis_1", "roc", "density_turnover",  "density_diversity")
@@ -14,10 +22,10 @@ data_for_h2 <-
   unnest(data_merge) %>% 
   dplyr::select(all_of(select_vars)) %>%
   left_join(data_meta %>% 
-              dplyr::select(dataset_id, lat, long, region, ecozone_koppen_5),
+              dplyr::select(dataset_id, lat, long, region, ecozone_koppen_15),
             by = "dataset_id") %>%
   drop_na() %>%
-  nest(data = -c("age", "ecozone_koppen_5", "region")) %>%
+  nest(data = -c("age", "ecozone_koppen_15", "region")) %>%
   dplyr::mutate(n_samples = purrr::map_dbl(data, ~nrow(.x))) %>%
   dplyr::filter(n_samples > 4)
 
@@ -28,7 +36,7 @@ pap_procrustes_ecozones <- data_for_h2 %>%
                                    .f = run_pca)) %>%
   mutate(pca_analysis = pca_analysis %>% 
            rlang::set_names(nm = data_for_h2$age))  %>% 
-  group_by(region, ecozone_koppen_5) %>%
+  group_by(region, ecozone_koppen_15) %>%
   summarise(pca_analysis = list(pca_analysis)) %>% 
   mutate(m2 = purrr::map(pca_analysis, get_procrustes_m2))%>%
   ungroup() %>%
@@ -40,11 +48,10 @@ pap_procrustes_ecozones <- data_for_h2 %>%
 # Add principal coordinate analysis of similarities and differences with time
 pcoa_ecozones <- 
   pap_procrustes_ecozones %>%
-  filter(!c(region == "North America" & ecozone_koppen_5 == "Tropical")) %>%
   mutate(PCoA = purrr::map(m2, .f = run_pcoa)) %>%
   mutate(site_scores = purrr::pmap(list(PCoA,
                                         region,
-                                        ecozone_koppen_5),
+                                        ecozone_koppen_15),
                                    .f = ~get_pcoa_scores(pcoa = ..1,
                                                     region = ..2,
                                                     ecozone = ..3))) %>%
@@ -56,10 +63,10 @@ pcoa_ecozones <-
 pcoa_ecozones %>% 
   dplyr::select(site_scores) %>% 
   unnest(cols = c(site_scores)) %>%
-  ggplot(aes(x = X1, y = X2, label = label, col = ecozone_koppen_5)) +
+  ggplot(aes(x = X1, y = X2, label = label, col = ecozone)) +
   coord_fixed() +
-  geom_hline(yintercept = 0, linetype = "dashed", col = "grey", size = 1) +
-  geom_vline(xintercept = 0, linetype = "dashed", col = "grey", size = 1) +
+  geom_hline(yintercept = 0, linetype = "dashed", col = "grey", linewidth = 1) +
+  geom_vline(xintercept = 0, linetype = "dashed", col = "grey", linewidth = 1) +
   geom_point() + 
   geom_text(hjust = 0, nudge_x = 0.005) +
   labs(x = "PCoA1", y = "PCoA2") +
@@ -69,9 +76,9 @@ pcoa_ecozones %>%
 
 # PLOT change in m2 in consecutive time steps
 pcoa_ecozones %>%
-  dplyr::select(m2_time_df, region, ecozone_koppen_5) %>%
+  dplyr::select(m2_time_df, region, ecozone_koppen_15) %>%
   unnest(cols = c(m2_time_df)) %>%
-  ggplot(aes(x = as.numeric(time), y = delta_m2, col = ecozone_koppen_5, fill = ecozone_koppen_5 )) +
+  ggplot(aes(x = as.numeric(time), y = delta_m2, col = ecozone_koppen_15, fill = ecozone_koppen_15 )) +
   geom_point() +
   geom_smooth() +
   scale_x_reverse() +
@@ -80,37 +87,3 @@ pcoa_ecozones %>%
 
 
 
-## 
-
-# function to get the full procustes analysis in time order?
-# Need this to extract the information about changes in interrelationship variables 
-
-
-get_protest_time <- function(data_list) {
-  
-  name_vec <- names(data_list)
-  
-  protest_list <- list()
-  for (i in 1:length(data_list)) {
-    protest1 <- NULL
-    for (j in 1:length(data_list)) {
-      
-      protest2 <- ifelse(j > i, NA, vegan::protest(X = data_list[[i]], 
-                                                   Y = data_list[[j]], 
-                                                   scale = TRUE, 
-                                                   symmetric = TRUE, 
-                                                   scores = "species",
-                                                   permutation = 499))
-      
-      protest1 <- list(protest1, protest2)
-    }
-    protest_list <- c(protest_list, protest1)
-  }
-  
-  
-  
-  return(protest_list)
-  
-}
-
-get_protest_time(pap_procrustes_ecozones$pca_analysis[[1]])
