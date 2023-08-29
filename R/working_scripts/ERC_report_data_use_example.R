c65c75b005b087ffe198dc414393f87170c51bc6
Kuber Bhatta <47561250+kbh022@users.noreply.github.com>
2023-08-29 11:45:57 +0200

    code of spatial temporal turnover

--- R/working_scripts/ERC_report_data_use_example.R
+++ R/working_scripts/ERC_report_data_use_example.R
@@ -46,22 +46,29 @@ data_dcca <-
         sel_complexity = "poly_2",
         transform_to_percentage = FALSE,
         tranformation = "none"
-      )
-    ),
+        )
+      ),
     dcca_grad_length = purrr::map_dbl(
       .x = dcca,
       .f = ~ .x %>%
         purrr::pluck("axis_1_grad_length")
+      ),
+    dcca_scores = purrr::map(
+      .x = dcca,
+      .f = ~ .x %>% 
+        purrr::pluck("case_r")
       )
     ) %>%
   dplyr::select(dataset_id,
                 long,
                 lat,
+                levels,
                 ecozone_koppen_5,
-                dcca_grad_length)
+                dcca_grad_length,
+                dcca_scores
+                )
 
 # Base map with Köppen-Geiger 5 climate zones (in Beck et al. 2018)
-
 base_map <-
   data_dcca %>%
   ggplot2::ggplot(ggplot2::aes(x = long,
@@ -87,11 +94,214 @@ plot_dcca <-
     error_family = "mgcv::Tweedie(p = 1.1, link = 'log')"
     )
 
+#ggsave(
+#  plot_dcca,
+#  file = paste(
+#    "Data_summary_outputs/Figure/",
+#    "Europe_data_filtered_gradient_length_250823.tiff",
+#    sep = ""
+#    ),
+#  dpi = 400,
+#  compress = "lzw",
+#  width = 15,
+#  height = 15,
+#  unit = "cm"
+#  )
+
+
+data_axis1 <-
+  data_dcca %>%
+  dplyr::mutate(
+    dcca_scores =
+      purrr::map2(
+        .x = dcca_scores,
+        .y = levels,
+        .f = ~ .x %>%
+          dplyr::select(sample_id,
+                        axis_1) %>%
+          inner_join(.y %>%
+                       dplyr::select(sample_id,
+                                     age),
+                     by = "sample_id")
+        )
+    ) %>% 
+  dplyr::select(dataset_id, 
+                long,
+                lat,
+                climate_zone = ecozone_koppen_5,
+                dcca_scores) %>% 
+  tidyr::unnest(dcca_scores) %>% 
+  dplyr::mutate_at("dataset_id", as.factor)
+
+climate_zone_vec <- 
+  c("Cold", "Temperate", "Polar", "Arid") %>% 
+  rlang::set_names()
+
+
+data_mod <-
+  purrr::walk(
+    .x = climate_zone_vec, 
+    .f = ~ {
+      sel_ecozone <- .x
+      
+      sel_data <-
+        data_axis1 %>%
+        dplyr::filter(climate_zone == sel_ecozone) 
+      
+      message(sel_ecozone)
+      
+      sel_data$dataset_id %>%
+        unique() %>%
+        length() %>%
+        message()
+      
+      if (
+        nrow(sel_data) > 0
+      ) {
+        # Fit GAM model
+        data_mod <-
+          REcopol::fit_hgam(
+            x_var = "age",
+            y_var = "axis_1",
+            group_var = "dataset_id",
+            smooth_basis = "tp",
+            data_source = sel_data,
+            error_family = "mgcv::Tweedie(p = 1.1)",
+            sel_k = 10, 
+            common_trend = TRUE,
+            use_parallel = FALSE
+          )
+        readr::write_rds(
+          x = data_mod,
+          file = paste0(
+            here::here(
+              "Data_summary_outputs/Data/Models_per_ecozone"
+            ),
+            "/",
+            sel_ecozone,
+            ".rds"
+          ),
+          compress = "gz"
+        )
+        
+        return(data_mod)
+      }
+    }
+  )
+
+# Predict the models
+data_mod <-
+  purrr::map(
+    .x = climate_zone_vec,
+    .f = ~ readr::read_rds(
+      file = paste0(
+        here::here("Data_summary_outputs/Data/Models_per_ecozone"),
+               "/",
+               .x,
+               ".rds"
+             )
+      )
+    )
+
+data_ecozone_predicted <-
+  purrr::map_dfr(
+    .x = climate_zone_vec,
+    .id = "climate_zone",
+    .f = ~ {
+      sel_ecozone <- .x
+      
+      message(sel_ecozone)
+      
+      sel_data <-
+        data_axis1 %>%
+        dplyr::filter(climate_zone == sel_ecozone) 
+      
+      # New data for predicting
+      new_data_general <-
+        tibble::tibble(age = seq(0, 12000, by = 100))
+      data_pred <-
+        REcopol::predic_model(
+          model_source = data_mod[[sel_ecozone]],
+          data_source = new_data_general %>%
+            dplyr::mutate(dataset_id = sel_data$dataset_id[1]),
+          exclude_var = data_mod[[sel_ecozone]] %>%
+            gratia::smooths() %>%
+            stringr::str_subset(., "dataset_id")
+          ) %>%
+        dplyr::rename(DCCA_axis_1 = fit)
+      return(data_pred)
+      }
+    )
+
+
+plot_temporal <-
+  data_ecozone_predicted %>%
+  ggplot2::ggplot(
+    ggplot2::aes(
+      x = age,
+      y = DCCA_axis_1,
+      group = climate_zone
+      )
+    ) +
+  ggplot2::geom_ribbon(
+    data = data_ecozone_predicted,
+    ggplot2::aes(
+      ymin = lwr,
+      ymax = upr,
+      fill = climate_zone,
+      ),
+    colour = NA,
+    alpha = 0.2
+    ) + 
+  ggplot2::geom_line(
+    data = data_ecozone_predicted,
+    ggplot2::aes(
+      colour = climate_zone
+      ),
+    size = 1
+    ) + 
+  ggplot2::theme_classic() +
+  ggplot2::scale_fill_manual(
+    values = palette_ecozone 
+  ) +
+  ggplot2::scale_color_manual(
+    values = palette_ecozone 
+  ) + 
+  ggplot2::labs(
+    x = "Time (cal yr BP)",
+    y = "DCCA axis-1"
+    ) +
+  ggplot2::scale_x_continuous(
+    breaks = seq(0, 12000, 2000),
+    labels = seq(0, 12000, 2000)
+    ) +
+  
+  labs(fill = "Climate zone",
+       colour = "Climate zone") + 
+  
+  ggplot2::theme(
+    axis.text.x = ggplot2::element_text(
+      color = "black",
+      size = 12,
+      angle = 45,
+      hjust = 1
+    ),
+    axis.text.y = ggplot2::element_text(
+      color = "black",
+      size = 12
+    ),
+    axis.title = ggplot2::element_text(
+      color = "black",
+      size = 14
+    ),
+    legend.position = "bottom"
+  )
+
 ggsave(
-  plot_dcca,
+  plot_temporal,
   file = paste(
     "Data_summary_outputs/Figure/",
-    "Europe_data_filtered_gradient_length_250823.tiff",
+    "Europe_data_filtered_turnover_temporal_250823.tiff",
     sep = ""
     ),
   dpi = 400,
