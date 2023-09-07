9fc1ae363d6f55c784c5bf469be59329f9f2e802
Vivian Astrup Felde <vivian.felde@uib.no>
2023-09-07 16:55:05 +0200

    fixed function; change in name

---
+++ R/functions/validation/run_mvpart.R
@@ -0,0 +1,37 @@
+# function to run MVPART model 
+
+run_mvpart <- function(data, 
+                       preds = "age", 
+                       trans = TRUE, 
+                       method = "gower", 
+                       ...) {
+  
+  if(isTRUE(trans)){
+    trans <- vegan::vegdist(data %>% 
+                              dplyr::select(n0:density_diversity),
+                            method = method) 
+    
+    input <- data.matrix(trans)
+    
+    res <- mvpart::mvpart(form = formula(paste("input ~", preds)),
+                          xv = "1se", 
+                          xval = nrow(data),
+                          xvmult = 100,
+                       #   xv =  "1se",
+                          all.leaves = TRUE,
+                          data = data.frame(data))
+  } else{
+    
+    input <- data.matrix(data %>% dplyr::select(n0:density_diversity))
+    res <- mvpart::mvpart(form = formula(paste("input ~", preds)),
+                          xv = "1se", 
+                          xval = nrow(data),
+                          xvmult = 100,
+                       #   xv =  "1se",
+                          all.leaves = TRUE,
+                          data = data.frame(data))  
+  }
+  
+  return(res)
+  
+}
