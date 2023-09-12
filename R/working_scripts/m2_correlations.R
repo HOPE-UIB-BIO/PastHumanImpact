242398420c31b9467ebaf97191bf62c6c6589c6e
Vivian Astrup Felde <vivian.felde@uib.no>
2023-09-12 13:40:14 +0200

    remove function

--- R/working_scripts/m2_correlations.R
+++ R/working_scripts/m2_correlations.R
@@ -9,7 +9,8 @@ data_meta <-
                     "_targets_h1"
                                ))
 
-data_meta <- data_meta %>%
+data_meta <- 
+  data_meta %>%
   dplyr::mutate(
     sel_classification = dplyr::case_when(
       ecozone_koppen_15 == "Cold_Without_dry_season" ~ ecozone_koppen_30,
@@ -37,66 +38,6 @@ palette_ecozones <-
 
 
 
-get_data_properties <- function(data_source_diversity,
-                                data_source_roc,
-                                data_source_density,
-                                used_rescale = TRUE) {
-  
-  
-  
-  if(
-    isTRUE(used_rescale)
-  ) {
-    data_source_density <- data_source_density %>%
-      dplyr::select(dataset_id, pap_density_rescale) %>%
-      dplyr::rename(density = pap_density_rescale)
-  } else {
-    data_source_density <- data_source_density %>%
-      dplyr::select(dataset_id, pap_density) %>%
-      dplyr::rename(density = pap_density)
-    
-  }
-  
-  dplyr::inner_join(
-    data_source_diversity %>%
-      dplyr::rename(
-        diversity = data
-      ),
-    data_source_roc %>%
-      dplyr::rename(
-        roc = data
-      ),
-    by = "dataset_id"
-  ) %>%
-    dplyr::inner_join(
-      data_source_density,
-      by = "dataset_id"
-    ) %>%
-    dplyr::mutate(
-      data_merge = purrr::pmap(
-        .l = list(
-          diversity, # ..1
-          roc, # ..2
-          density #..3
-        ),
-        .f = ~ dplyr::inner_join(
-          ..1,
-          ..2,
-          by = "age"
-        ) %>%
-          dplyr::inner_join(
-            ..3,
-            by = "age"
-          )  %>%
-          drop_na()
-      ) 
-    ) %>%
-    dplyr::select(
-      dataset_id, data_merge
-    ) %>%
-    
-    return()
-}
 
 # get full data for properties (but interpolated)
 data_properties <- 
