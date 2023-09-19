0f03f15cada56581408da060e7d2b290f394810d
Vivian Astrup Felde <vivian.felde@uib.no>
2023-09-19 15:31:00 +0200

    change in theme and scales

--- R/functions/visualisation/get_predictor_barplot.R
+++ R/functions/visualisation/get_predictor_barplot.R
@@ -1,9 +1,10 @@
 # plot predictor bars of temporal analysis
 
-get_predictor_barplot <- function(data, 
-                                   sel_predictor = "human",
-                                   x_var = "percentage_median",
-                                   axis_to_right = TRUE) {
+get_predictor_barplot <- 
+  function(data, 
+           sel_predictor = "human",
+           x_var = "percentage_median",
+           axis_to_right = TRUE) {
   
   
   p_predictor <-  
@@ -33,20 +34,32 @@ get_predictor_barplot <- function(data,
       aes(
         y = as.factor(age/1000),
         x = get(x_var),
-        fill = predictor),
+        fill = predictor
+        ),
       stat = "identity",
       width = .6,   
       alpha = 0.4, 
       show.legend = FALSE
     ) +
     scale_fill_manual(
-      values = palette_pred
+      values = palette_pred,
+      drop = FALSE
     ) +
     scale_x_continuous(name = NULL,
                        limits = c(0, 100),
-                       breaks = seq(0, 100, by = 20),
+                       breaks = seq(0, 100, by = 25),
                        expand = c(0,0)) +
-    scale_y_discrete(limits = rev) +
+    scale_y_discrete(limits = rev,
+                     drop = FALSE) +
+ 
+    theme(
+      panel.background  = element_rect(fill = "transparent", color = NA),
+      plot.background = element_rect(fill = "transparent", color = NA),
+      line = element_line(linewidth = 0.01),
+      text = element_text(size = 8, color = "grey30"),
+      axis.text.x = element_text(size = 8, angle = 60),
+      plot.margin = grid::unit(c(0, 0, 0, 0), "mm")
+    ) +
     labs(y = "",
          x = "")
   
@@ -59,7 +72,9 @@ get_predictor_barplot <- function(data,
                       limits = c(100, 0),
                       breaks = seq(0, 100, by = 20),
                       expand = c(0,0)) +
-      scale_y_discrete(limits = rev, position = "right" )
+      scale_y_discrete(limits = rev, 
+                       position = "right",
+                       drop = FALSE) 
     
   } else {
     
