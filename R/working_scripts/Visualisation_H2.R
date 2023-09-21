eeccd688e61a6fa3708f6a0e8a4d8458705c7c0b
Vivian Astrup Felde <vivian.felde@uib.no>
2023-09-21 17:08:54 +0200

    fix figure H2 and map inserts

--- R/working_scripts/Visualisation_H2.R
+++ R/working_scripts/Visualisation_H2.R
@@ -3,6 +3,7 @@
 ## FIGURE 3: RESULTS H2
 ###########################################################
 # Import results & tables for plotting
+library(patchwork)
 
 source("R/working_scripts/Results_script.R")
 
@@ -176,49 +177,43 @@ x_label <- c("Human",  "Time", "Climate")
 
 
 # GET LISTS OF REGIONAL MAPS FOR DIFFERENT PALETTE
-select_region = "North America"
-
-map_region_polar <- get_map_region(select_region = select_region,
-                                   col_vec = palette_polar)
-map_region_cold <- get_map_region(select_region = select_region,
-                                  col_vec = palette_cold)
-map_region_temp <- get_map_region(select_region = select_region,
-                                  col_vec = palette_temperate)
-map_region_arid <- get_map_region(select_region = select_region,
-                                  col_vec = palette_arid)
-map_region_tropical <- get_map_region(select_region = select_region,
-                                      col_vec = palette_tropical)
-
-NA_map_list <- list(polar = map_region_polar, 
-                    cold = map_region_cold, 
-                    temp = map_region_temp, 
-                    arid = map_region_arid,
-                    tropical = map_region_tropical)
 
 
-LA_map_list <- list(polar = map_region_polar, 
-                    cold = map_region_cold, 
-                    temp = map_region_temp, 
-                    arid = map_region_arid,
-                    tropical = map_region_tropical)
+list_maps <- function(select_region = "Europe") {
+  
+  map_region_polar <- get_map_region(select_region = select_region,
+                                     col_vec = palette_polar)
+  map_region_cold <- get_map_region(select_region = select_region,
+                                    col_vec = palette_cold)
+  map_region_temp <- get_map_region(select_region = select_region,
+                                    col_vec = palette_temperate)
+  map_region_arid <- get_map_region(select_region = select_region,
+                                    col_vec = palette_arid)
+  map_region_tropical <- get_map_region(select_region = select_region,
+                                        col_vec = palette_tropical)
+  
+  map_list <- list(polar = map_region_polar, 
+                   cold = map_region_cold, 
+                   temp = map_region_temp, 
+                   arid = map_region_arid,
+                   tropical = map_region_tropical)
+  
+  return(map_list)
+  
+  
+}
 
-Europe_map_list <- list(polar = map_region_polar, 
-                        cold = map_region_cold, 
-                        temp = map_region_temp, 
-                        arid = map_region_arid,
-                        tropical = map_region_tropical)
 
-Asia_map_list <- list(polar = map_region_polar, 
-                      cold = map_region_cold, 
-                      temp = map_region_temp, 
-                      arid = map_region_arid,
-                      tropical = map_region_tropical)
+NA_map_list <- list_maps(select_region = "North America")
 
-oceania_map_list <- list(polar = map_region_polar, 
-                         cold = map_region_cold, 
-                         temp = map_region_temp, 
-                         arid = map_region_arid,
-                         tropical = map_region_tropical)
+
+LA_map_list <- list_maps(select_region = "Latin America")
+
+Europe_map_list <- list_maps(select_region = "Europe")
+
+Asia_map_list <- list_maps(select_region = "Asia")
+
+oceania_map_list <- list_maps(select_region = "Oceania")
 
 
 # GET FIGURES CIRCULAR BARS
@@ -282,7 +277,7 @@ m2_change_region <-
                   color = NA), 
                 panel.grid.major = element_line(
                   color = "grey90", 
-                  linewidth = 0.01), 
+                  linewidth = 0.1), 
                 axis.title.y = element_text(size = 6),
                 axis.text.x = element_text(size = 6, angle = 60),
                 axis.text.y = element_text(size = 6),
@@ -315,207 +310,178 @@ names(m2_change_region) <- name_list
 # COMBINE FIGURES
 
 
-
-circular_bar_h2 %>% 
-  names()
+# to shift themes
 
 
+remove_axis_labels <-  theme(axis.title.x = element_blank(),
+                      axis.text.x = element_blank(),
+                      axis.title.y = element_blank(),
+                      axis.text.y = element_blank())
 
-# Vectors to extract regions from lists
+remove_axis_labels_x <- theme(axis.title.x = element_blank(),
+                              axis.text.x = element_blank())
 
-NorthA <- 
-  m2_change_region %>% 
-  names() %>% 
-  grepl("North America", .)
+remove_axis_labels_y <- theme(axis.title.y = element_blank(),
+                             axis.text.y = element_blank())
 
-LatinA <- 
-  m2_change_region %>% 
-  names() %>% 
-  grepl("Latin America", .)
-
-Europe <- 
-  m2_change_region %>% 
-  names() %>% 
-  grepl("Europe", .)
-
-Asia <- 
-  m2_change_region %>% 
-  names() %>% 
-  grepl("Asia", .)
-
-Oceania <- 
-  m2_change_region %>% 
-  names() %>% 
-  grepl("Oceania", .)
+remove_space <- theme(plot.margin = unit(c(0,-2,0,-2), "mm"))
 
 # PUT TOGETHER FIGURES PER REGION
 
-NA_p1 <- 
-  cowplot::plot_grid(
-    circular_bar_h2[["North America"]],
-    plotlist = m2_change_region[NorthA], 
-    nrow = 1,
-    rel_widths = c(2, 1, 1, 1, 1))
 
-NA_h2 <-
-  cowplot::ggdraw(NA_p1) +
-  cowplot::draw_plot(NA_map_list$arid, 
-                     x = 0.365, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(NA_map_list$cold, 
-                     x = 0.535, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(NA_map_list$polar, 
-                     x = 0.7, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(NA_map_list$temp, 
-                     x = 0.855, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2)
+dev.new(width = 180, height = 70, units = "mm") 
+
+
+
+
+NA_p1 <-
+  circular_bar_h2$`North America` +
+  m2_change_region$`North America_Arid` + 
+  remove_axis_labels_x +
+  inset_element(NA_map_list$arid, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`North America_Polar` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(NA_map_list$polar, left = 0.365, bottom = 0.7, right = 1, top = 1) +  
+  m2_change_region$`North America_Cold` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(NA_map_list$cold, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`North America_Temperate` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(NA_map_list$temp, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  plot_layout(nrow = 1)
+
+
 
 LA_p1 <- 
-  cowplot::plot_grid(
-    circular_bar_h2[["Latin America"]],
-    plotlist = m2_change_region[LatinA], 
-    nrow = 1,
-    rel_widths = c(2, 1, 1, 1, 1))
-
-LA_h2 <-
-  cowplot::ggdraw(LA_p1) +
-  cowplot::draw_plot(LA_map_list$arid,
-                     x = 0.365, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(LA_map_list$polar, 
-                     x = 0.535, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(LA_map_list$temp, 
-                     x = 0.7, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(LA_map_list$tropical, 
-                     x = 0.85, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2)
+  circular_bar_h2$`Latin America` +
+  m2_change_region$`Latin America_Arid` + 
+  remove_axis_labels_x +
+  inset_element(LA_map_list$arid, left = 0.365, bottom = 0.7, right = 1, top = 0.9) +
+  m2_change_region$`Latin America_Polar` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(LA_map_list$polar, left = 0.365, bottom = 0.7, right = 1, top = 0.9) +
+  m2_change_region$`Latin America_Temperate` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(LA_map_list$temp, left = 0.365, bottom = 0.7, right = 1, top = 0.9) +
+  m2_change_region$`Latin America_Tropical` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(LA_map_list$tropical, left = 0.365, bottom = 0.7, right = 1, top = 0.9) +
+  plot_layout(nrow = 1)
+ 
 
 europe_p1 <- 
-  cowplot::plot_grid(
-    circular_bar_h2[["Europe"]],
-    plotlist = m2_change_region[Europe], 
-    nrow = 1,
-    rel_widths = c(2, 1, 1, 1, 1))
+  circular_bar_h2$`Europe` +
+  m2_change_region$`Europe_Arid` + 
+  remove_axis_labels_x +
+  inset_element(Europe_map_list$arid, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`Europe_Polar` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(Europe_map_list$polar, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`Europe_Temperate` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(Europe_map_list$temp, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`Europe_Cold` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(Europe_map_list$cold, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  plot_layout(nrow = 1)
 
-europe_h2 <-
-  cowplot::ggdraw(europe_p1) +
-  cowplot::draw_plot(Europe_map_list$arid, 
-                     x = 0.365, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(Europe_map_list$cold, 
-                     x = 0.535, 
-                     y = 0.7, 
-                     width = 0.2, 
-                     height = .2) +
-  cowplot::draw_plot(Europe_map_list$polar, 
-                     x = 0.7, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(Europe_map_list$temp, 
-                     x = 0.855, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2)
+
 
 asia_p1 <- 
-  cowplot::plot_grid(
-    circular_bar_h2[["Asia"]],
-    plotlist = m2_change_region[Asia], 
-    nrow = 1,
-    rel_widths = c(2, 1, 1, 1, 1))
+  circular_bar_h2$`Asia` +
+  m2_change_region$`Asia_Arid` + 
+  inset_element(Asia_map_list$arid, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`Asia_Polar` +
+  remove_axis_labels_y +
+  remove_space +
+  inset_element(Asia_map_list$polar, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`Asia_Temperate` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(Asia_map_list$temp, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  m2_change_region$`Asia_Cold` +
+  remove_axis_labels +
+  remove_space +
+  inset_element(Asia_map_list$cold, left = 0.365, bottom = 0.7, right = 1, top = 1) +
+  plot_layout(nrow = 1)
 
-asia_h2 <-
-  cowplot::ggdraw(asia_p1) +
-  cowplot::draw_plot(Asia_map_list$arid, 
-                     x = 0.365, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(Asia_map_list$cold, 
-                     x = 0.535, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(Asia_map_list$polar, 
-                     x = 0.7, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(Asia_map_list$temp, 
-                     x = 0.855, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2)
+ 
 
 oceania_p1 <- 
-  cowplot::plot_grid(
-    circular_bar_h2[["Oceania"]],
-    plotlist = m2_change_region[Oceania], 
-    "NULL",
-    "NULL",
-    nrow = 1,
-    rel_widths = c(2, 1, 1, 1, 1))
+  circular_bar_h2$`Oceania` +
+  plot_spacer() +
+  plot_spacer() +
+  m2_change_region$`Oceania_Temperate` +
+  remove_axis_labels_y +
+  inset_element(oceania_map_list$temp, left = 0.365, bottom = 0.7, right = 1, top = 0.99) +
+  plot_spacer() +
+  plot_layout(nrow = 1)
 
-oceania_h2 <-
-  cowplot::ggdraw(oceania_p1) +
-  cowplot::draw_plot(oceania_map_list$temp, 
-                     x = 0.7, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2) +
-  cowplot::draw_plot(oceania_map_list$tropical, 
-                     x = 0.855, 
-                     y = 0.7, 
-                     width = .2, 
-                     height = .2)
+# FINAL COMBINED
+layout <- c(
+  area(t = 1, l = 1, b = 1, r = 5),
+  area(t = 2, l = 1, b = 2, r = 5),
+  area(t = 3, l = 1, b = 3, r = 5),
+  area(t = 4, l = 1, b = 4, r = 5),
+  area(t = 5, l = 1, b = 5, r = 5)
+)
 
+plot(layout)
+
+
+combine_plots <- 
+ (NA_p1 / 
+  LA_p1 / 
+  europe_p1 /
+  asia_p1 / 
+  oceania_p1) + 
+  plot_layout(design = layout)
 
 
 # FINAL COMBINE ALL REGIONS
 combine_h2 <-
-  cowplot::plot_grid(NA_h2, 
-                     LA_h2,
-                     europe_h2,
-                     asia_h2,
-                     oceania_h2,
-                     ncol = 1)
+  cowplot::plot_grid(NA_p1, 
+                     LA_p1, 
+                     europe_p1, 
+                     asia_p1, 
+                     oceania_p1, 
+                     ncol = 1, 
+                     align = "hv",
+                     axis = 1) 
+  
+combine_h2
+
+dev.off()
 
 # SAVE
 
-pdf("combined_h2_version2.pdf", width = 7, height = 7.5)  
-combine_h2  
+pdf("combined_h2_version3.pdf", width = 9, height = 9, unit = "cm")  
+print(combine_h2)  
 dev.off()
 
-system2('open', args = 'combined_h2_version2.pdf')
+system2('open', args = 'combined_h2_version3.pdf')
+
+png("combined_plot_h2.png", width = 9, height = 9, unit = "cm", dpi = 300)
+print(combined)
+
+dev.off()
+
+system2('open', args = 'combined_h2_version3.pdf')
 
 ggsave(
-  paste0("m2_change_time_,", select_region, ".png"),
-  plot_m2,
-  width = 8, height = 6, units = "cm",
-  scaling = 0.5,
+  "combined_figure_h2.png",
+  combine_h2,
+  width = 18, height = 7.5, units = "cm",
+  dpi = 500,
   bg = "white"
 )
 
