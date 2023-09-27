7319b03ca3632ea2a052dd7a8f2ec1cae6537426
OndrejMottl <ondrej.mottl@gmail.com>
2023-09-27 09:35:07 +0200

    get_predictor_barplot: replace axis with filled bar

--- R/functions/visualisation/get_predictor_barplot.R
+++ R/functions/visualisation/get_predictor_barplot.R
@@ -1,21 +1,38 @@
 # plot predictor bars of temporal analysis
 
 get_predictor_barplot <-
-  function(data,
+  function(data_source,
            sel_predictor = "human",
            x_var = "percentage_median",
            axis_to_right = TRUE,
            sel_palette) {
+    data_age_dummy <-
+      data_source %>%
+      dplyr::filter(predictor == sel_predictor) %>%
+      dplyr::distinct(age, predictor)
+
     p_predictor <-
-      data %>%
+      tibble::tibble() %>%
       ggplot2::ggplot() +
       ggplot2::geom_bar(
-        data = data %>%
+        data = data_age_dummy,
+        mapping = ggplot2::aes(
+          y = as.factor(age / 1000),
+          x = 100
+        ),
+        fill = "gray90",
+        stat = "identity",
+        width = 0.6,
+        alpha = 1,
+        show.legend = FALSE
+      ) +
+      ggplot2::geom_bar(
+        data = data_source %>%
           dplyr::filter(
             variance_partition == "Unique_percent_median",
             predictor %in% sel_predictor
           ),
-        ggplot2::aes(
+        mapping = ggplot2::aes(
           y = as.factor(age / 1000),
           x = get(x_var),
           fill = predictor
@@ -26,7 +43,7 @@ get_predictor_barplot <-
         show.legend = FALSE
       ) +
       ggplot2::geom_bar(
-        data = data %>%
+        data = data_source %>%
           dplyr::filter(
             variance_partition == "Average.share_percent_median",
             predictor %in% sel_predictor
@@ -53,7 +70,8 @@ get_predictor_barplot <-
       ) +
       ggplot2::scale_y_discrete(
         limits = rev,
-        drop = FALSE
+        drop = FALSE,
+        breaks = as.factor(seq(0, 10, 2))
       ) +
       ggplot2::theme(
         panel.background = ggplot2::element_rect(
@@ -64,14 +82,11 @@ get_predictor_barplot <-
         ),
         line = ggplot2::element_line(linewidth = 0.01),
         text = ggplot2::element_text(size = 8, color = "grey30"),
-        axis.text.x = ggplot2::element_text(size = 8, angle = 60),
-        plot.margin = grid::unit(c(0, 0, 0, 0), "mm")
-      ) +
-      ggplot2::labs(
-        y = "",
-        x = ""
+        plot.margin = grid::unit(c(0, 0, 0, 0), "mm"),
+        axis.text.x = ggplot2::element_blank(),
+        axis.ticks.x = ggplot2::element_blank(),
+        axis.title = ggplot2::element_blank()
       )
-
 
     if (isTRUE(axis_to_right)) {
       p_predictor <-
@@ -85,7 +100,8 @@ get_predictor_barplot <-
         ggplot2::scale_y_discrete(
           limits = rev,
           position = "right",
-          drop = FALSE
+          drop = FALSE,
+          breaks = as.factor(seq(0, 10, 2))
         )
     } else {
       p_predictor
