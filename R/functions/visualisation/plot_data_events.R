68f18333c9a1de0f365cafd785df7346a24be57f
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-10-03 21:38:36 +0200

    plot_data_events: add `sel_classification`, remove duplicated code

--- R/functions/visualisation/plot_data_events.R
+++ R/functions/visualisation/plot_data_events.R
@@ -1,51 +1,62 @@
 plot_data_events <- function(data_source_events,
                              select_region = NULL,
                              data_raw = TRUE) {
+  data_meta_work <-
+    targets::tar_read(
+      name = "data_meta",
+      store = paste0(
+        data_storage_path,
+        "_targets_h1"
+      )
+    ) %>%
+    dplyr::mutate(
+      sel_classification = dplyr::case_when(
+        ecozone_koppen_15 == "Cold_Without_dry_season" ~ ecozone_koppen_30,
+        ecozone_koppen_5 == "Cold" ~ ecozone_koppen_15,
+        ecozone_koppen_5 == "Temperate" ~ ecozone_koppen_15,
+        .default = ecozone_koppen_5
+      )
+    ) %>%
+    dplyr::mutate(
+      sel_classification = as.factor(sel_classification)
+    ) %>%
+    dplyr::full_join(
+      data_climate_zones, # [config criteria]
+      .,
+      by = "sel_classification"
+    ) %>%
+    dplyr::mutate(
+      region = factor(region,
+        levels = vec_regions # [config criteria]
+      )
+    ) %>%
+    dplyr::filter(
+      region != "Africa"
+    ) %>%
+    dplyr::select(
+      region,
+      sel_classification,
+      dataset_id,
+      long, lat
+    )
+
+  data_work <-
+    data_source_events %>%
+    tidyr::unnest(data_to_fit) %>%
+    dplyr::inner_join(
+      data_meta_work,
+      by = "dataset_id"
+    )
+
   if (
     isTRUE(data_raw)
   ) {
-    data <-
-      data_source_events %>%
-      tidyr::unnest(data_to_fit) %>%
-      dplyr::inner_join(
-        targets::tar_read(
-          name = "data_meta",
-          store = paste0(
-            data_storage_path,
-            "_targets_h1"
-          )
-        ) %>%
-          dplyr::select(
-            dataset_id,
-            long,
-            lat,
-            region,
-            ecozone_koppen_15
-          ),
-        by = "dataset_id"
-      ) %>%
+    data_to_plot <-
+      data_work %>%
       dplyr::filter(region == select_region)
   } else {
-    data <-
-      data_source_events %>%
-      tidyr::unnest(data) %>%
-      dplyr::inner_join(
-        targets::tar_read(
-          name = "data_meta",
-          store = paste0(
-            data_storage_path,
-            "_targets_h1"
-          )
-        ) %>%
-          dplyr::select(
-            dataset_id,
-            long,
-            lat,
-            region,
-            ecozone_koppen_15
-          ),
-        by = "dataset_id"
-      ) %>%
+    data_to_plot <-
+      data_work %>%
       tidyr::pivot_longer(
         bi:ei,
         names_to = "var_name",
@@ -56,7 +67,7 @@ plot_data_events <- function(data_source_events,
   }
 
   fig <-
-    data %>%
+    data_to_plot %>%
     ggplot2::ggplot(
       mapping = ggplot2::aes(
         x = age,
@@ -64,8 +75,11 @@ plot_data_events <- function(data_source_events,
         col = var_name
       )
     ) +
-    ggplot2::geom_point() +
+    ggplot2::facet_wrap(~sel_classification) +
     ggplot2::scale_colour_hue(c = 50, l = 50, h = c(30, 300)) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.position = "bottom") +
+    ggplot2::labs(title = select_region, x = "") +
     ggplot2::geom_smooth(
       method = "gam",
       se = FALSE,
@@ -74,11 +88,8 @@ plot_data_events <- function(data_source_events,
         family =
           stats::binomial(link = "logit")
       )
-    ) +
-    ggplot2::facet_wrap(~ecozone_koppen_15) +
-    ggplot2::theme_classic() +
-    ggplot2::theme(legend.position = "bottom") +
-    ggplot2::labs(title = select_region, x = "")
+    )
+
 
   return(fig)
 }
