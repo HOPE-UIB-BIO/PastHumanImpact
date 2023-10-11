45cf328817883c9f906ecad2f3fa10eebcf644e6
OndrejMottl <ondrej.mottl@gmail.com>
2023-10-11 11:09:50 +0200

    get_circular_barplot: replace panel grid with custom lines, add icons

--- R/functions/visualisation/get_circular_barplot.R
+++ R/functions/visualisation/get_circular_barplot.R
@@ -14,13 +14,12 @@ get_circular_barplot <- function(data_source,
     ggplot2::ggplot(
       mapping = ggplot2::aes(
         x = get(x_var),
-        y = get(y_var),
-        fill = get(fill_var)
+        y = get(y_var)
       )
     ) +
     ggplot2::scale_y_continuous(
-      limits = c(-5, 40.1),
       expand = c(0, 0),
+      limits = c(-5, NA),
       breaks = seq(0, 50, by = y_step)
     ) +
     ggplot2::scale_x_discrete(
@@ -36,30 +35,29 @@ get_circular_barplot <- function(data_source,
         fill = background_col, color = NA
       ),
       panel.border = ggplot2::element_blank(),
-      panel.grid.major.y = ggplot2::element_line(
-        color = line_col,
-        linewidth = line_width
-      ),
-      panel.grid.major.x = ggplot2::element_line(
-        color = line_col,
-        linetype = "dashed",
-        linewidth = line_width
-      ),
-      panel.grid.minor = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
       axis.line = ggplot2::element_blank(),
       axis.title = ggplot2::element_blank(),
       axis.ticks = ggplot2::element_blank(),
-      axis.text.y = ggplot2::element_blank(),
-      axis.text.x = ggplot2::element_text(
-        size = text_size,
-        vjust = -3
-      ),
+      axis.text = ggplot2::element_blank(),
       plot.margin = grid::unit(c(0, 0, 0, 0), "mm"),
       panel.spacing = grid::unit(c(0, 0, 0, 0), "mm")
     ) +
     ggplot2::scale_fill_manual(
       values = col_vec,
       drop = FALSE
+    ) +
+    # add lines for every 10 percent
+    ggplot2::geom_hline(
+      yintercept = seq(0, 40, by = y_step),
+      color = line_col,
+      linewidth = line_width
+    ) +
+    ggplot2::geom_vline(
+      xintercept = (0:3) + 0.5,
+      linetype = "dashed",
+      linewidth = line_width,
+      color = line_col
     ) +
     ggplot2::annotate(
       "text",
@@ -69,17 +67,6 @@ get_circular_barplot <- function(data_source,
       vjust = 0,
       size = 1.5
     ) +
-    # add lines for every 10 percent
-    # ggplot2::geom_hline(
-    #  yintercept = seq(0, 50, by = y_step),
-    #  color = "grey90"
-    # ) +
-    #    ggplot2::geom_vline(
-    #  xintercept = (0:3) + 0.5,
-    #  linetype = "dashed",
-    #  linewidth = 0.01,
-    #  color = "grey90"
-    # ) +
     ggplot2::geom_col(
       data = data_source %>%
         dplyr::filter(
@@ -88,6 +75,9 @@ get_circular_barplot <- function(data_source,
             variance_partition
           )
         ),
+      mapping = ggplot2::aes(
+        fill = get(fill_var)
+      ),
       width = 0.6,
       position = ggplot2::position_dodge2(
         width = 0.8,
@@ -101,12 +91,39 @@ get_circular_barplot <- function(data_source,
           "average_share_percent",
           variance_partition
         )),
+      mapping = ggplot2::aes(
+        fill = get(fill_var)
+      ),
       width = .6,
       position = ggplot2::position_dodge2(
         width = 0.8,
         preserve = "single"
       ),
       alpha = 0.4
+    ) +
+    ggimage::geom_image(
+      data = data.frame(
+        x = c(0.75, 2, 3.25),
+        y = rep(45, 3),
+        image = c(
+          here::here(
+            "Outputs/icons/human.png"
+          ),
+          here::here(
+            "Outputs/icons/time.png"
+          ),
+          here::here(
+            "Outputs/icons/climate.png"
+          )
+        )
+      ),
+      mapping = ggplot2::aes(
+        x = x,
+        y = y,
+        image = image
+      ),
+      size = 0.2,
+      alpha = 1
     ) +
     ggplot2::coord_polar()
 
