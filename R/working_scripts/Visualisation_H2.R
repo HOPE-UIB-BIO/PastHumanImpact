eef6aad4e9942b70b594cabadbb0ad5e572ffe72
OndrejMottl <ondrej.mottl@gmail.com>
2023-10-11 13:33:21 +0200

    Figure 4: limit map insets to only how seleced region, add icons, fix bug with truckatin data

--- R/working_scripts/Visualisation_H2.R
+++ R/working_scripts/Visualisation_H2.R
@@ -52,7 +52,8 @@ list_maps <- function(select_region) {
     rlang::set_names() %>%
     purrr::map(
       .f = ~ get_map_region(
-        rasterdata = data_geo_koppen,
+        rasterdata = data_geo_koppen %>%
+          dplyr::filter(region == select_region),
         select_region = select_region,
         sel_palette = get_greyout_palette(.x)
       )
@@ -137,6 +138,24 @@ data_geo_koppen <-
       ecozone_koppen_5 == "Temperate" ~ ecozone_koppen_15,
       .default = ecozone_koppen_5
     )
+  ) %>%
+  dplyr::distinct(x, y, .keep_all = TRUE) %>%
+  dplyr::filter(
+    x < 175.5 & x > -175 & y < 90 & y > -90
+  ) %>%
+  dplyr::mutate(
+    long = x,
+    lat = y
+  ) %>%
+  RUtilpol::geo_assign_value(
+    data_source = .,
+    dir = paste0(
+      data_storage_path, "Data/Regions_shapefile/"
+    ),
+    sel_method = "shapefile",
+    file_name = "Regions",
+    var = "region",
+    var_name = "region"
   )
 
 
@@ -174,7 +193,8 @@ list_region_maps_grey <-
   vec_regions %>% # [config criteria]
   purrr::map(
     .f = ~ get_map_region(
-      rasterdata = data_geo_koppen,
+      rasterdata = data_geo_koppen %>%
+        dplyr::filter(region == .x),
       select_region = .x,
       sel_palette = palette_ecozones, # [config criteria]
       sel_alpha = 0
@@ -208,6 +228,7 @@ data_circular_bar_h2 <-
         x_var = "predictor",
         line_width = 0.2,
         line_col = "grey75",
+        icon_size = 0.2,
         col_vec = palette_ecozones, # [config criteria]
         x_name = predictors_label # [config criteria]
       )
