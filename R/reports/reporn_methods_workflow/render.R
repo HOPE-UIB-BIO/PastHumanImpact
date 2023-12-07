4fe48a8d6f0bf91e2a40b20d63b47c0ebf532846
OndrejMottl <ondrej.mottl@gmail.com>
2023-12-07 14:49:56 +0100

    add small script to render the report

---
+++ R/reports/reporn_methods_workflow/render.R
@@ -0,0 +1,45 @@
+#----------------------------------------------------------#
+#
+#
+#                     GlobalHumanImpact
+#
+#                  render report -workflow
+#
+#
+#                   O. Mottl, V. Felde
+#                         2023
+#
+#----------------------------------------------------------#
+
+#----------------------------------------------------------#
+# 0. Setup -----
+#----------------------------------------------------------#
+
+library(here)
+
+# Load configuration
+source(
+  here::here(
+    "R/00_Config_file.R"
+  )
+)
+
+library(quarto)
+library(rmarkdown)
+
+#----------------------------------------------------------#
+# 1. render -----
+#----------------------------------------------------------#
+
+rmarkdown::render(
+  input = here::here(
+    "R/reports/reporn_methods_workflow/report_methodology_workflow.qmd"
+  ),
+  output_file = here::here(
+    "R/reports/reporn_methods_workflow/report_methodology_workflow.html"
+  ),
+  output_format = rmarkdown::html_document(
+    self_contained = TRUE,
+    fig_width = 10
+  )
+)
