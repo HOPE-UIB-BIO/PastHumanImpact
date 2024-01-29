09ea63c8f00590d3e9f04e05fb186abc7374f6e2
Vivian Astrup Felde <vivian.felde@uib.no>
2024-01-29 23:40:53 +0100

    targets that has been moved to predictor pipeline, keep combine and filter data and the analyses

--- R/project/06_pipeline_hvar_spatial_temporal.R
+++ R/project/06_pipeline_hvar_spatial_temporal.R
@@ -29,12 +29,12 @@ source(
 # - Load meta data
 source(
   here::here(
-    "R/project/01_meta_data.R"
+    "R/project/02_meta_data.R"
   )
 )
 
 # - Setting for targets
-Sys.setenv(TAR_PROJECT = "project")
+Sys.setenv(TAR_PROJECT = "_targets_h1")
 
 targets::tar_option_set(
   packages = package_list, # [config]
@@ -45,127 +45,51 @@ targets::tar_option_set(
   storage = "worker"
 )
 
+# - Load data from _targets_data
+
+data_predictors <-   
+  targets::tar_read(
+  name = "data_predictors",
+  store = paste0(
+    data_storage_path,
+    "_targets_data"
+  )
+)
+
+data_properties <-   
+  targets::tar_read(
+    name = "data_properties",
+    store = paste0(
+      data_storage_path,
+      "_targets_data"
+    )
+  )
+
 #----------------------------------------------------------#
 # 1. Targets -----
 #----------------------------------------------------------#
 
 list(
-  # - Load climate data ----
-  targets::tar_target(
-    name = file_climate_path,
-    command = paste0(
-      data_storage_path,
-      "HOPE_Hypothesis1/Data/climate/data_climate-2024-01-23.rds"
-    ),
-    format = "file"
-  ),
-  # - load data from path
-  targets::tar_target(
-    name = data_climate,
-    command = get_data(file_climate_path)
-  ),
-  # - select climate variables
-  targets::tar_target(
-    name = data_climate_for_interpolation,
-    command = get_climate_data_for_interpolation(
-      data_source = data_climate,
-      sel_var = c(
-        "temp_annual",
-        "temp_cold",
-        "prec_annual",
-        "prec_summer",
-        "prec_win"
-      )
-    )
-  ),
-  # - interpolate climate data for each time slice
-  targets::tar_target(
-    name = data_climate_interpolated,
-    command = get_interpolated_data(
-      data_source = data_climate_for_interpolation,
-      variable = "var_name",
-      vars_interpolate = c("age", "value"),
-      group_var = "dataset_id",
-      method = "linear",
-      rule = 1,
-      ties = mean,
-      age_min = 0,
-      age_max = 12e03,
-      timestep = 500,
-      verbose = TRUE
-    )
-  ),
-  # - Load spd data ----
-  targets::tar_target(
-    name = file_spd_path,
-    command = paste0(
-      data_storage_path,
-      "HOPE_Hypothesis1/Data/spd/data_spd_processed-2024-01-23.rds"
-    ),
-    format = "file"
-  ),
-  # - load data from path
-  targets::tar_target(
-    name = data_spd,
-    command = get_data(file_spd_path)
-  ),
-  # - prepare spd for modelling
-  targets::tar_target(
-    name = data_spd_to_fit,
-    command = get_spd_for_modelling(data_spd)
-  ),
-  # - get interpolated spd values for each time slice
-  targets::tar_target(
-    name = data_spd_interpolated,
-    command = get_interpolated_data(
-      data_source = data_spd_to_fit,
-      variable = "var_name",
-      vars_interpolate = c("age", "value"),
-      group_var = "dataset_id",
-      method = "linear",
-      rule = 1,
-      ties = mean,
-      age_min = 0,
-      age_max = 12e03,
-      timestep = 500,
-      verbose = TRUE
-    )
-  ),
-  # - combine spd and human impact data ---- 
-  targets::tar_target(
-    name = data_spd_events,
-    command = get_events_spd_combined(
-      data_source_events = events_temporal_subset,
-      data_source_spd = data_spd_interpolated,
-      data_source_meta = data_meta,
-      data_source_dummy_time = data_dummy_time,
-      select_spd_distance = 250
-    )
-  ),
-  # - combine all data together 
+  # - combine properties and predictors ----
   targets::tar_target(
     name = data_all_combined,
-    command = get_data_for_hvarpar(
-      data_source_diversity = data_div_dcca_interpolated,
-      data_source_roc = data_roc_interpolated,
-      data_source_density = data_density_estimate,
-      data_source_spd = data_spd_events,
-      data_source_climate = data_climate_interpolated,
-      used_rescale = TRUE
+    command = get_data_combined(
+      data_source_properties = data_properties,
+      data_source_predictors = data_predictors
     )
   ),
-  # - filter data by age ranges, remove private data
+  # - filter data for analyses ----
   targets::tar_target(
-    name = data_hvar_filtered,
-    command = get_data_hvar_filtered(
+    name = data_hvar,
+    command = get_data_hvar(
       data_source = data_all_combined,
       age_min = 2000,
       age_max = 8500,
       remove_private = TRUE
     )
   ),
-  # - Run analyses: Hierarchical variation partitioning 
-  # - spatial (within core) analysis with spd
+  # - Hierarchical variation partitioning: ----
+  # - run spatial (within core) analysis with spd ----
   targets::tar_target(
     name = output_spatial_spd,
     command = run_hvarpart(
@@ -198,7 +122,7 @@ list(
       permutations = 999
     )
   ),
-  # - spatial (within core) analysis with events
+  # - run spatial (within core) analysis with events ----
   targets::tar_target(
     name = output_spatial_events,
     command = run_hvarpart(
@@ -241,7 +165,7 @@ list(
       permutations = 999
     )
   ),
-  # - run temporal analysis (by age bins in spatial context) with spd
+  # - run temporal analysis with spd ----
   targets::tar_target(
     name = output_temporal_spd,
     command = run_hvarpart(
@@ -273,7 +197,7 @@ list(
       permutations = 999
     )
   ),
-  # - run temporal analysis (by age bins in spatial context) with events
+  # - run temporal analysis with events ----
   targets::tar_target(
     name = output_temporal_events,
     command = run_hvarpart(
