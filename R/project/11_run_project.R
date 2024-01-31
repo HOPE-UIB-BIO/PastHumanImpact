0acd41c5d322bbb29edcffd55b0a17fbf1498dc2
Vivian Astrup Felde <vivian.felde@uib.no>
2024-01-31 11:17:27 +0100

    add pipeline checks

--- R/project/11_run_project.R
+++ R/project/11_run_project.R
@@ -37,6 +37,17 @@ targets::tar_make(
      )
  )
 
+# check pipeline pollen data
+targets::tar_visnetwork(
+  targets_only = TRUE,
+  script = here::here("R/project/03_pipeline_pollen_data.R"),
+  store = paste0(
+    data_storage_path,
+    "_targets_data"
+  )
+)
+
+
 #----------------------------------------------------------#
 # 2. run target pipeline events data -----
 #----------------------------------------------------------#
@@ -48,10 +59,30 @@ targets::tar_make(
      )
  )
 
+# check pipeline events data
+targets::tar_visnetwork(
+  targets_only = TRUE,
+  script = here::here("R/project/04_pipeline_events.R"),
+  store = paste0(
+    data_storage_path,
+    "_targets_data"
+  )
+)
+
 #----------------------------------------------------------#
 # 3. run target pipeline predictors -----
 #----------------------------------------------------------#
 targets::tar_make(
+  script = here::here("R/project/05_pipeline_predictors.R"),
+  store = paste0(
+    data_storage_path,
+    "_targets_data"
+  )
+)
+
+# check pipeline predictors
+targets::tar_visnetwork(
+  targets_only = TRUE,
   script = here::here("R/project/05_pipeline_predictors.R"),
   store = paste0(
     data_storage_path,
@@ -69,6 +100,16 @@ targets::tar_make(
          "_targets_h1"
      )
  )
+
+# check hvar spatial & temporal (h1)
+targets::tar_visnetwork(
+  targets_only = TRUE,
+  script = here::here("R/project/06_pipeline_hvar_spatial_temporal.R"),
+  store = paste0(
+    data_storage_path,
+    "_targets_h1"
+  )
+)
 
 #----------------------------------------------------------#
 # 5. run target pipeline multidimensional shifts (h2) -----
