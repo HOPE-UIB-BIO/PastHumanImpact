7ed614119279e9eeb9294be62d93e1d9f9e85f63
Vivian Astrup Felde <vivian.felde@uib.no>
2024-01-31 15:01:37 +0100

    fix get filtered data function

--- R/functions/hvarpart/get_data_filtered.R
+++ R/functions/hvarpart/get_data_filtered.R
@@ -1,30 +1,32 @@
-get_data_filtered <- function(data_source_combined,
-                              data_source_meta,
+get_data_filtered <- function(data_source,
+                              data_meta,
                               age_from = 2000,
                               age_to = 8500,
                               remove_private = TRUE){
   
-  # - filter datasets 
-  if(isTRUE(remove_private)){
-    data_filtered <- 
-      data_source_combined %>%
-      dplyr::left_join(data_meta %>%
-                         dplyr::select(dataset_id, 
-                                       region, 
-                                       data_publicity),
-                       by = "dataset_id") %>%
-      dplyr::filter(!(region == "Latin America" & data_publicity == "private")) 
+  
+    # - filter datasets 
+    if(isTRUE(remove_private)){
+      keep_dataset <- 
+        data_meta %>%
+        dplyr::select(dataset_id, 
+                      region, 
+                      data_publicity) %>%
+        dplyr::filter(!(region == "Latin America" 
+                        & data_publicity == "private")) %>% 
+        pluck("dataset_id")
+      
     } else {
-      data_filtered <- data_source_combined
+      keep_dataset <- meta_data$dataset_id
     }
     
   # - filter age limits within datasets
-  data_filtered <- 
-    data_filter %>%
+  data_filtered %>%
+    dplyr::filter(dataset_id %in% keep_dataset) %>%
     tidyr::unnest(data_merge) %>%
     dplyr::filter(
-            age >= age_from & age <= age_to
-          ) %>%
+      age >= age_from & age <= age_to
+    ) %>%
     tidyr::nest(data_merge = -c(dataset_id)) %>%
     return()
     
