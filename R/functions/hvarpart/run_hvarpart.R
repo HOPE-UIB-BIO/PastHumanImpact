013a64169c2ed84b9a4ff5b56fe9de66749844c5
Vivian Astrup Felde <vivian.felde@uib.no>
2024-02-01 14:45:06 +0100

    update in vars

--- R/functions/hvarpart/run_hvarpart.R
+++ R/functions/hvarpart/run_hvarpart.R
@@ -18,20 +18,26 @@
 #' @param permutations integer; number of permutations for p-values
 
 run_hvarpart <- function(data_source,
+                         response_dist = NULL,
                          response_vars = c(
-                           "n0", "n1", "n2",
-                           "n1_minus_n2", "n2_divided_by_n1", "n1_divided_by_n0",
+                           "n0", 
+                           "n1", 
+                           "n2",
+                           "n1_minus_n2", 
+                           "n2_divided_by_n1", 
+                           "n1_divided_by_n0",
                            "roc",
-                           "dcca_axis_1"
+                           "dcca_axis_1",
+                           "density_diversity", 
+                           "density_turnover"
                          ),
-                         responce_dist = NULL,
                          predictor_vars = list(
                            human = c("spd"),
                            climate = c(
+                             "temp_annual",
                              "temp_cold",
                              "prec_summer",
-                             "prec_win",
-                             "gdm"
+                             "prec_win"
                            ),
                            time = c("age")
                          ),
@@ -39,7 +45,7 @@ run_hvarpart <- function(data_source,
                          time_series = TRUE,
                          get_significance = TRUE,
                          permutations = 99) {
-  res <- NULL
+ res <- NULL
 
   if (
     isFALSE(is.null(response_vars))
@@ -61,27 +67,28 @@ run_hvarpart <- function(data_source,
         )
       )
   } else {
-    res <-
-      data_source %>%
-      dplyr::rename(
-        responce_dist = eval(responce_dist)
-      ) %>%
-      dplyr::mutate(
-        varhp = purrr::map2(
-          .x = data_merge,
-          .y = responce_dist,
-          .f = ~ get_varhp(
-            data_source = .x,
-            permutations = permutations,
-            response_vars = NULL,
-            responce_dist = .y,
-            predictor_vars = predictor_vars,
-            run_all_predictors = run_all_predictors,
-            time_series = time_series,
-            get_significance = get_significance
-          )
-        )
-      )
+  
+   res <-
+     data_source %>%
+     dplyr::rename(
+       response_dist = eval(response_dist)
+     ) %>%
+     dplyr::mutate(
+       varhp = purrr::map2(
+         .x = data_merge,
+         .y = response_dist,
+         .f = ~ get_varhp(
+           data_source = .x,
+           permutations = permutations,
+           response_vars = NULL,
+           response_dist = .y,
+           predictor_vars = predictor_vars,
+           run_all_predictors = run_all_predictors,
+           time_series = time_series,
+           get_significance = get_significance
+         )
+       )
+     )
   }
 
   return(res)
