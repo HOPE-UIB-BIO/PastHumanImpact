3710798a712f03633a247e51558336d529bb593e
Vivian Astrup Felde <vivian.felde@uib.no>
2024-02-01 10:32:24 +0100

    fix bug in reshaping data for time bins

--- R/functions/hvarpart/get_data_timebin.R
+++ R/functions/hvarpart/get_data_timebin.R
@@ -1,17 +1,13 @@
 get_data_timebin <- function(data_source,
-                                  data_meta) {
+                             data_meta) {
   
   data_source %>%
-    unnest(data_merge) %>%
-    drop_na() %>%
-    left_join(data_meta %>% 
-                dplyr::select(dataset_id, lat, long, region),
-              by = "dataset_id") %>%
-    nest(data_merge = -c(region, age)) %>%
-    mutate(n_samples = purrr::map_dbl(data_merge, 
-                                      .f = function(x){ 
-                                        nrow(x) %>% 
-                                          return()})) %>%
+    tidyr::unnest(data_merge) %>%
+    dplyr::left_join(data_meta %>% 
+                       dplyr::select(dataset_id, lat, long, region),
+                     by = "dataset_id") %>%
+    tidyr::nest(data_merge = -c(region, age)) %>%
+    mutate(n_samples = purrr::map_int(data_merge, nrow))  %>%
     return()
  
 }
