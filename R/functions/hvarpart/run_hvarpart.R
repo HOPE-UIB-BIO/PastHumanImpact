00fe65852666298c791bf27762e5fc4a3a0807c1
Vivian-Felde <vivian.felde@uib.no>
2024-02-02 17:32:11 +0100

    remove last bit; fixed the get var_hp to choose among distances in vegdist

--- R/functions/hvarpart/run_hvarpart.R
+++ R/functions/hvarpart/run_hvarpart.R
@@ -1,13 +1,9 @@
 #' @title Run hierarchial variation partitioning
 #' @description This wrapper run the hierarchical variation
 #' partitioning for the HOPE dataset.
-#' There is an oprion to use already calculated distance matrix by selecting
-#' `reponse_vars` == NULL, and `responce_dist` == [name of a column]
 #' @param data_source input tibble
 #' @param reponse_vars
 #' vector of variables to be included in the response datasetet
-#' @param responce_dist
-#' A name of column with already pre-calcuated distance
 #' @param predictor_vars
 #' vector of names of predictor variables or list with names
 #' @param run_all_predictors logical; if predictor variables should be assessed
@@ -45,11 +41,10 @@ run_hvarpart <- function(data_source,
                          time_series = TRUE,
                          get_significance = TRUE,
                          permutations = 99) {
- res <- NULL
+ 
+  res <- NULL
 
-  if (
-    isFALSE(is.null(response_vars))
-  ) {
+ 
     res <-
       data_source %>%
       dplyr::mutate(
@@ -66,30 +61,6 @@ run_hvarpart <- function(data_source,
           )
         )
       )
-  } else {
-  
-   res <-
-     data_source %>%
-     dplyr::rename(
-       response_dist = eval(response_dist)
-     ) %>%
-     dplyr::mutate(
-       varhp = purrr::map2(
-         .x = data_merge,
-         .y = response_dist,
-         .f = ~ get_varhp(
-           data_source = .x,
-           permutations = permutations,
-           response_vars = NULL,
-           response_dist = .y,
-           predictor_vars = predictor_vars,
-           run_all_predictors = run_all_predictors,
-           time_series = time_series,
-           get_significance = get_significance
-         )
-       )
-     )
-  }
 
   return(res)
 }
