24e27d7a02ef64496319d2faed1b78e78739c885
OndrejMottl <ondrej.mottl@gmail.com>
2024-02-02 14:40:13 +0100

    01_prepare_data: filter out redundant data, rename script and data file

--- R/project/predictor_models/01_prepare_data.R
+++ R/project/predictor_models/01_prepare_data.R
@@ -57,7 +57,7 @@ if (
 # 2. Data wrangling -----
 #----------------------------------------------------------#
 
-data_to_fit <-
+data_merge <-
   dplyr::inner_join(
     regions, # loaded from 02_meta_data.R
     climate_zones, # loaded from 02_meta_data.R
@@ -87,6 +87,58 @@ if (
     split(.$variable) %>%
     purrr::map(~ summary(.x))
 }
+
+valid_climate_zones_by_n_records <-
+  data_merge %>%
+  dplyr::group_by(region, climatezone) %>%
+  dplyr::distinct(dataset_id) %>%
+  dplyr::summarise(
+    .groups = "drop",
+    n_records = dplyr::n()
+  ) %>%
+  dplyr::filter(
+    n_records >= min_n_records_per_climate_zone # [config]
+  )
+
+data_filter_by_climatezone <-
+  data_merge %>%
+  dplyr::inner_join(
+    valid_climate_zones_by_n_records,
+    by = c("region", "climatezone")
+  )
+
+data_constant_variables <-
+  data_filter_by_climatezone %>%
+  dplyr::distinct(region, climatezone, variable) %>%
+  dplyr::mutate(
+    is_it_constant = purrr::pmap_lgl(
+      .progress = TRUE,
+      .l = list(region, climatezone, variable),
+      .f = ~ {
+        data_filter_by_climatezone %>%
+          dplyr::filter(
+            region == ..1 &
+              climatezone == ..2 &
+              variable == ..3
+          ) %>%
+          dplyr::distinct(value) %>%
+          nrow() == 1
+      }
+    )
+  ) %>%
+  dplyr::filter(is_it_constant) %>%
+  dplyr::select(-is_it_constant)
+
+data_filter_out_constant <-
+  data_filter_by_climatezone %>%
+  dplyr::anti_join(
+    data_constant_variables,
+    by = c("region", "climatezone", "variable")
+  )
+
+data_prepared <-
+  data_filter_out_constant %>%
+  dplyr::select(-n_records)
 
 #----------------------------------------------------------#
 # 3. Visualisation -----
@@ -122,8 +174,8 @@ if (
 #----------------------------------------------------------#
 
 RUtilpol::save_latest_file(
-  object_to_save = data_to_fit,
-  file_name = "predictor_models_data_to_fit",
+  object_to_save = data_prepared,
+  file_name = "predictor_models_data_prepared",
   dir = paste0(
     data_storage_path,
     "Data/Predictor_models/"
