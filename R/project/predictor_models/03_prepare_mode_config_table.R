52548ff510887fd58141e987f84a4e43f7d6bf0c
OndrejMottl <ondrej.mottl@gmail.com>
2024-02-04 14:19:44 +0100

    03_prepare_mode_config_table: remove error family (as it is in data to fit), increase iteration across the board

--- R/project/predictor_models/03_prepare_mode_config_table.R
+++ R/project/predictor_models/03_prepare_mode_config_table.R
@@ -44,10 +44,10 @@ data_to_fit <-
 
 model_config_table <-
   data_to_fit %>%
-  dplyr::distinct(region, climatezone, variable, n_records, error_family) %>%
+  dplyr::distinct(region, climatezone, variable, n_records) %>%
   dplyr::mutate(
-    total_iterations = 2000,
-    min_iterations_per_chain = 50,
+    total_iterations = 4000,
+    min_iterations_per_chain = 100,
     last_run_date = NA_character_,
     last_run_time = NA_character_,
     last_run_rhat_test_pass = FALSE,
