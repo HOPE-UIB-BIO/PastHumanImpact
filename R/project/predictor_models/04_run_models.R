6f6d91d16bd91dde34a7962fc0f9c1054ce7be3d
OndrejMottl <ondrej.mottl@gmail.com>
2024-02-05 15:54:08 +0100

    04_run_models: small fixes in debugging code, add comments

--- R/project/predictor_models/04_run_models.R
+++ R/project/predictor_models/04_run_models.R
@@ -66,7 +66,8 @@ purrr::pwalk(
     models_to_run$climatezone, # ..2
     models_to_run$variable # ..3
   ),
-  .f = {
+  .f = ~ {
+    # save as obejct for easier debugging
     sel_region <- ..1
     sel_climatezone <- ..2
     sel_variable <- ..3
@@ -83,6 +84,7 @@ purrr::pwalk(
       # start with the smallest model first
       dplyr::arrange(n_records)
 
+    # get the selected model
     sel_mod <-
       models_to_run %>%
       dplyr::filter(
@@ -91,6 +93,7 @@ purrr::pwalk(
           variable == sel_variable
       )
 
+    # check if the model needs to be run
     if (
       sel_mod$need_to_run[[1]] == FALSE
     ) {
@@ -99,6 +102,7 @@ purrr::pwalk(
 
     current_env <- environment()
 
+    # subset the data for the selected model
     sel_data_filtered <-
       data_to_fit %>%
       dplyr::filter(
@@ -107,24 +111,34 @@ purrr::pwalk(
           variable == sel_variable
       )
 
+    # get the error family
     sel_error_family <-
       sel_data_filtered %>%
       purrr::pluck("error_family", 1)
 
+    # get the raw data
     sel_data_to_fit <-
       sel_data_filtered %>%
       purrr::chuck("data_to_fit", 1)
 
+    # get the priors
     sel_data_prior <-
       sel_data_filtered %>%
       purrr::chuck("priors", 1)
 
+    # get the total number of iterations
     sel_set_total_iter <-
-      sel_mod$total_iterations[[1]]
+      sel_mod %>%
+      purrr::chuck("total_iterations", 1)
 
+    # get the minimum number of iterations per chain
     sel_set_min_iter_per_chain <-
-      sel_mod$min_iterations_per_chain[[1]]
+      sel_mod %>%
+      purrr::chuck("min_iterations_per_chain", 1)
 
+
+    # Small subroutine to decrease the number of cores used if the number
+    #   of iterations per chain is too small
     if (
       (sel_set_total_iter / n_cores_available) < sel_set_min_iter_per_chain
     ) {
@@ -135,9 +149,23 @@ purrr::pwalk(
         n_cores_available
     }
 
+    # calculate the number of iterations per chain
     sel_iter_per_chain <-
       ceiling(sel_set_total_iter / n_cores_to_use)
 
+    # print final settings
+    message(
+      paste(
+        "Will fit model for",
+        sel_region, "in", sel_climatezone, "for", sel_variable, "\n",
+        "- Using cores:", n_cores_to_use, "\n",
+        "- error family:", sel_error_family, "\n",
+        "- total number of iterations:", sel_set_total_iter, "\n",
+        "- number of iterations per chain:", sel_iter_per_chain, "\n"
+      )
+    )
+
+    # start time
     time_mod_start <- Sys.time()
 
     # Run model
@@ -152,8 +180,10 @@ purrr::pwalk(
         control = list(adapt_delta = 0.9)
       )
 
+    # end time
     time_mod_end <- Sys.time()
 
+    # if there is and obejct
     if (
       exists("mod", envir = current_env)
     ) {
@@ -161,28 +191,30 @@ purrr::pwalk(
         models_to_run %>%
         dplyr::mutate(
           last_run_date = dplyr::case_when(
-            .default = last_run_date,
+            .default = as.character(last_run_date),
             region == sel_region &
               climatezone == sel_climatezone &
               variable == sel_variable ~ as.character(Sys.Date())
           ),
           last_run_start_time = dplyr::case_when(
-            .default = last_run_start_time,
+            .default = as.character(last_run_start_time),
             region == sel_region &
               climatezone == sel_climatezone &
               variable == sel_variable ~ as.character(time_mod_start)
           ),
           last_run_end_time = dplyr::case_when(
-            .default = last_run_end_time,
+            .default = as.character(last_run_end_time),
             region == sel_region &
               climatezone == sel_climatezone &
               variable == sel_variable ~ as.character(time_mod_end)
           ),
           last_run_time = dplyr::case_when(
-            .default = last_run_time,
+            .default = as.character(last_run_time),
             region == sel_region &
               climatezone == sel_climatezone &
-              variable == sel_variable ~ as.character(time_mod_end - time_mod_start)
+              variable == sel_variable ~ as.character(
+              time_mod_end - time_mod_start
+            )
           ),
           need_to_be_evaluated = dplyr::case_when(
             .default = need_to_be_evaluated,
@@ -198,6 +230,7 @@ purrr::pwalk(
           )
         )
 
+      # save the updated table
       RUtilpol::save_latest_file(
         object_to_save = models_to_run_updated,
         file_name = "predictor_models_config_table",
@@ -208,8 +241,9 @@ purrr::pwalk(
         prefered_format = "csv"
       )
 
+      # save the model
       RUtilpol::save_latest_file(
-        object_to_save = sel_mod,
+        object_to_save = mod,
         file_name = paste(
           sel_variable,
           sel_region,
