8a1f4882cfdaa33172a1d60a2bb89efc6e7d1b91
OndrejMottl <ondrej.mottl@gmail.com>
2024-03-04 12:15:19 +0100

    Visualisations_H1_new: lintr, formating and namespaces

--- R/working_scripts/Visualisations_H1_new.R
+++ R/working_scripts/Visualisations_H1_new.R
@@ -30,22 +30,24 @@ source(
 #----------------------------------------------------------#
 
 # - Load list of summary tables from pipeline spd
-summary_tables_spd <- targets::tar_read(
-  name = "summary_tables_spd",
-  store = paste0(
-    data_storage_path,
-    "_targets_data/analyses_h1"
+summary_tables_spd <-
+  targets::tar_read(
+    name = "summary_tables_spd",
+    store = paste0(
+      data_storage_path,
+      "_targets_data/analyses_h1"
+    )
   )
-)
 
 # - Load list of summary tables from events
-summary_tables_events <- targets::tar_read(
-  name = "summary_tables_events",
-  store = paste0(
-    data_storage_path,
-    "_targets_data/analyses_h1"
+summary_tables_events <-
+  targets::tar_read(
+    name = "summary_tables_events",
+    store = paste0(
+      data_storage_path,
+      "_targets_data/analyses_h1"
+    )
   )
-)
 
 #----------------------------------------------------------#
 # 2. Combine output data -----
@@ -53,36 +55,44 @@ summary_tables_events <- targets::tar_read(
 # Combine summary output for plotting ----
 data_for_plotting <-
   dplyr::inner_join(
-    summary_tables_spd$summary_spatial_long %>%
+    summary_tables_spd %>%
+      purrr::chuck("summary_spatial_long") %>%
       tidyr::nest(data_spatial = -c(region)),
-    summary_tables_spd$summary_temporal_long %>%
+    summary_tables_spd %>%
+      purrr::chuck("summary_temporal_long") %>%
       tidyr::nest(data_temporal_spd = -c(region)),
     by = "region"
   ) %>%
-  left_join(
-    summary_tables_events$summary_temporal_long %>%
+  dplyr::left_join(
+    summary_tables_events %>%
+      purrr::chuck("summary_temporal_long") %>%
       tidyr::nest(data_temporal_events = -c(region)),
     by = "region"
-  ) 
+  )
 
 # Combine temporal spd and events data for plotting ----
-data_source_temporal <- data_for_plotting %>%
+data_source_temporal <-
+  data_for_plotting %>%
   dplyr::select(region, data_temporal_spd) %>%
-  unnest(col=data_temporal_spd) %>%
+  unnest(col = data_temporal_spd) %>%
   mutate(human_pred = "spd") %>%
   full_join(
     data_for_plotting %>%
       dplyr::select(region, data_temporal_events) %>%
-      unnest(col=data_temporal_events) %>%
-      mutate(human_pred = "events")) %>%
-  mutate(predictor = factor(predictor, levels = c("human", "climate"))) 
+      unnest(col = data_temporal_events) %>%
+      mutate(human_pred = "events")
+  ) %>%
+  dplyr::mutate(
+    predictor = factor(predictor, levels = c("human", "climate"))
+  )
 
 # Distribution of ratios for individual records ----
-data_dist <- 
-  summary_tables_spd$summary_table_spatial %>%
+data_dist <-
+  summary_tables_spd %>%
+  purrr::chuck("summary_table_spatial") %>%
   dplyr::select(
     dataset_id,
-    region, 
+    region,
     climatezone,
     predictor,
     ratio_unique,
@@ -92,39 +102,42 @@ data_dist <-
   dplyr::mutate(
     predictor = factor(
       predictor,
-      levels = c("human", 
-                 "climate")
+      levels = c(
+        "human",
+        "climate"
       )
+    )
   ) %>%
   dplyr::mutate(
     climatezone = factor(climatezone)
   ) %>%
   tidyr::pivot_longer(
-    c(ratio_unique, 
-      ratio_ind),
+    c(
+      ratio_unique,
+      ratio_ind
+    ),
     names_to = "importance_type",
     values_to = "ratio"
   ) %>%
   dplyr::mutate(
     importance_type = factor(importance_type,
-                             levels = c(
-                               "ratio_unique",
-                               "ratio_ind"
-                               )
-                             )
-    ) 
+      levels = c(
+        "ratio_unique",
+        "ratio_ind"
+      )
+    )
+  )
 
 
 
 #----------------------------------------------------------#
 # 2. helper functions for plotting -----
 #----------------------------------------------------------#
-#temporal plot ----
+# temporal plot ----
 
 get_figure_temporal <- function(data_source_temporal) {
-  
   figure_temporal <-
-    data_source_temporal %>% 
+    data_source_temporal %>%
     ggplot2::ggplot() +
     ggplot2::geom_bar(
       data = data_source_temporal %>%
@@ -164,12 +177,11 @@ get_figure_temporal <- function(data_source_temporal) {
       ),
       show.legend = FALSE
     ) +
-    scale_y_continuous(
+    ggplot2::scale_y_continuous(
       limits = c(-0.2, 1.5),
       breaks = seq(-0.2, 1, 0.2)
     ) +
-    scale_x_discrete(limit = rev) +
-    
+    ggplot2::scale_x_discrete(limit = rev) +
     ggplot2::theme(
       panel.background = ggplot2::element_rect(
         fill = "transparent", color = NA
@@ -185,116 +197,125 @@ get_figure_temporal <- function(data_source_temporal) {
       axis.ticks.x = ggplot2::element_blank(),
       axis.title = ggplot2::element_text(size = 12, color = "grey30"),
       axis.line = ggplot2::element_blank()
-    )+
-    facet_wrap(~ predictor, ncol = 1) +
-    labs(x = "Age (ka) BP", y = "Ratio of importance") 
-  
+    ) +
+    ggplot2::facet_wrap(~predictor, ncol = 1) +
+    ggplot2::labs(x = "Age (ka) BP", y = "Ratio of importance")
+
   return(figure_temporal)
-  
 }
 
 # spatial plot ----
 get_spatial_barplot <- function(data_source_spatial) {
-  
-  figure_spatial <- 
-  data_source_spatial %>%
-  ggplot2::ggplot(
-    mapping = ggplot2::aes(
-      x = get("ratio"),
-      y = get("predictor"),
-      fill = get("climatezone")
-    )
-  ) +
-  ggplot2::theme(
-    legend.position = "none",
-    panel.background = ggplot2::element_rect(
-      fill = "white",
-      color = NA
-    ),
-    plot.background = ggplot2::element_rect(
-      fill = "white",
-      color = NA
-    ),
-    panel.border = ggplot2::element_blank(),
-    panel.grid.minor = ggplot2::element_blank(),
-    axis.line = ggplot2::element_blank(),
-    axis.title = ggplot2::element_blank(),
-    axis.ticks = ggplot2::element_blank(),
-    plot.margin = grid::unit(c(0, 0, 0, 0), "mm"),
-    panel.spacing = grid::unit(c(0, 0, 0, 0), "mm"),
-    text = ggplot2::element_text(size = 15, color = "grey30")
-  ) +
-  ggplot2::scale_fill_manual(
-    values = palette_ecozones,
-    drop = FALSE
-  ) +
-  
-  ggplot2::geom_col(
-    data = data_source_spatial %>%
-      dplyr::filter(
-        grepl(
-          "ratio_unique_wmean",
-          importance_type
-        )
+  figure_spatial <-
+    data_source_spatial %>%
+    ggplot2::ggplot(
+      mapping = ggplot2::aes(
+        x = get("ratio"),
+        y = get("predictor"),
+        fill = get("climatezone")
+      )
+    ) +
+    ggplot2::theme(
+      legend.position = "none",
+      panel.background = ggplot2::element_rect(
+        fill = "white",
+        color = NA
       ),
-    width = 0.6,
-    position = ggplot2::position_dodge2(
-      width = 0.8,
-      preserve = "single"
-    ),
-    alpha = 1
-  ) +
-  ggplot2::geom_col(
-    data = data_source_spatial %>%
-      dplyr::filter(grepl(
-        "ratio_ind_wmean",
-        importance_type
-      )),
-    width = .6,
-    position = ggplot2::position_dodge2(
-      width = 0.8,
-      preserve = "single"
-    ),
-    alpha = 0.4
-  )  
-return(figure_spatial)
+      plot.background = ggplot2::element_rect(
+        fill = "white",
+        color = NA
+      ),
+      panel.border = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.line = ggplot2::element_blank(),
+      axis.title = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      plot.margin = grid::unit(c(0, 0, 0, 0), "mm"),
+      panel.spacing = grid::unit(c(0, 0, 0, 0), "mm"),
+      text = ggplot2::element_text(size = 15, color = "grey30")
+    ) +
+    ggplot2::scale_fill_manual(
+      values = palette_ecozones,
+      drop = FALSE
+    ) +
+    ggplot2::geom_col(
+      data = data_source_spatial %>%
+        dplyr::filter(
+          grepl(
+            "ratio_unique_wmean",
+            importance_type
+          )
+        ),
+      width = 0.6,
+      position = ggplot2::position_dodge2(
+        width = 0.8,
+        preserve = "single"
+      ),
+      alpha = 1
+    ) +
+    ggplot2::geom_col(
+      data = data_source_spatial %>%
+        dplyr::filter(grepl(
+          "ratio_ind_wmean",
+          importance_type
+        )),
+      width = .6,
+      position = ggplot2::position_dodge2(
+        width = 0.8,
+        preserve = "single"
+      ),
+      alpha = 0.4
+    )
+  return(figure_spatial)
 }
 
-
-
-
-
 data_dist %>%
- # filter(n_records > 5) %>%
-  ggplot(aes(x = predictor, y  = ratio)) + 
+  # filter(n_records > 5) %>%
+  ggplot2::ggplot(
+    mapping = ggplot2::aes(
+      x = predictor,
+      y = ratio
+    )
+  ) +
   ggdist::stat_halfeye(
-    aes(color = predictor,
-        fill = after_scale(lighten(color, .1))),
+    mapping = ggplot2::aes(
+      color = predictor,
+      fill = after_scale(lighten(color, .1))
+    ),
     # adjust = .5,
     width = .5,
     .width = 0,
     justification = -.7,
-    point_colour = NA) +
-  geom_boxplot(
-    aes(color = climatezone,
-        color = after_scale(darken(color, .1)),
-        fill = after_scale(desaturate(lighten(color, .8), .4))
+    point_colour = NA
+  ) +
+  ggplot2::geom_boxplot(
+    mapping = ggplot2::aes(
+      color = climatezone,
+      color = ggplot2::after_scale(
+        colorspace::darken(color, .1)
+      ),
+      fill = ggplot2::after_scale(
+        desaturate(
+          colorspace::lighten(color, .8), .4
+        )
+      )
     ),
     width = .6,
     outlier.shape = NA
   ) +
-  
-  geom_point(
-    aes(color = climatezone,
-        color = after_scale(darken(color, .1))),
+  ggplot2::geom_point(
+    aes(
+      color = climatezone,
+      color = after_scale(darken(color, .1))
+    ),
     fill = "white",
     shape = 21,
     stroke = .4,
     size = 2,
     position = position_dodge(width = .6)
-  ) + 
-  geom_point(
-    aes(fill = climatezone),
+  ) +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(fill = climatezone),
     color = "transparent",
     shape = 21,
     stroke = .4,
@@ -302,16 +323,16 @@ data_dist %>%
     alpha = .3,
     position = position_dodge(width = .6)
   ) +
-  theme(
+  ggplot2::theme(
     legend.position = "none"
   ) +
-  scale_y_continuous(
+  ggplot2::scale_y_continuous(
     limits = c(-.2, 1.5),
     expand = c(0, 0),
-    breaks = seq(0,1.5, by = .2)
+    breaks = seq(0, 1.5, by = .2)
   ) +
-  scale_fill_manual(values = palette_ecozones) +
-  scale_color_manual(values = palette_ecozones) +
-  coord_flip() +
-  labs(x ="", y = "%") +
-  facet_wrap(~region, nrow = 1)
+  ggplot2::scale_fill_manual(values = palette_ecozones) +
+  ggplot2::scale_color_manual(values = palette_ecozones) +
+  ggplot2::coord_flip() +
+  ggplot2::labs(x = "", y = "%") +
+  ggplot2::facet_wrap(~region, nrow = 1)
