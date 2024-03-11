558ec48be1fa242ff9471921e6be7f7233387d51
Vivian-Felde <vivian.felde@uib.no>
2024-03-11 14:34:09 +0100

    fix name

--- R/project/06_pipeline_predictors.R
+++ R/project/06_pipeline_predictors.R
@@ -103,7 +103,7 @@ list(
     name = file_spd_path,
     command = paste0(
       data_storage_path,
-      "Data/spd/spd_combine_2024-03-11.rds"
+      "Data/spd/data_spd_combine_2024-03-11.rds"
     ),
     format = "file"
   ),
