1ca5550e5b9c14bc8198f076d09ffdd87ff9a56d
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2024-04-02 10:54:45 +0200

    Pred_models-LA_and_spd_fix (#269)

--- R/project/predictor_models/05_Evaluate_models.R
+++ R/project/predictor_models/05_Evaluate_models.R
@@ -31,44 +31,7 @@ loo_threshold <- 0.1
 rhat_threshold <- 1.1
 rhat_threshold_quantile <- 0.9
 
-# Function to flag the model to be rerun
-flag_model_to_rerun <- function(
-    data_source,
-    sel_region, sel_climatezone, sel_variable) {
-  models_to_run_updated <-
-    data_source %>%
-    dplyr::mutate(
-      need_to_be_evaluated = dplyr::case_when(
-        .default = need_to_be_evaluated,
-        region == sel_region &
-          climatezone == sel_climatezone &
-          variable == sel_variable ~ FALSE
-      ),
-      need_to_run = dplyr::case_when(
-        .default = need_to_run,
-        region == sel_region &
-          climatezone == sel_climatezone &
-          variable == sel_variable ~ TRUE
-      ),
-      last_evaluation_date = dplyr::case_when(
-        .default = as.character(last_evaluation_date),
-        region == sel_region &
-          climatezone == sel_climatezone &
-          variable == sel_variable ~ as.character(Sys.Date())
-      )
-    )
 
-  RUtilpol::save_latest_file(
-    object_to_save = models_to_run_updated,
-    file_name = "predictor_models_config_table",
-    dir = paste0(
-      data_storage_path,
-      "Data/Predictor_models/"
-    ),
-    prefered_format = "csv",
-    verbose = FALSE
-  )
-}
 
 #----------------------------------------------------------#
 # 0. Load data -----
