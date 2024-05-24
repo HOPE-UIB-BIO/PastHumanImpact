f2c674bcf6cc97a34355afeb79e784ae4b3882cd
Vivian-Felde <vivian.felde@uib.no>
2024-05-24 10:50:56 +0200

    update code, reshuffle and adjust main figure

--- R/project/extended_data_figures/EDA_pollen_data.R
+++ R/project/extended_data_figures/EDA_pollen_data.R
@@ -39,13 +39,17 @@ source(
 data_pollen <-
   targets::tar_read(
     name = data_assembly_filtered,
-    store = external_storage_targets
+    store = paste0(
+      data_storage_path,
+      "_targets_data/pipeline_pollen_data"
+    )
   ) %>%
   dplyr::select(
     dataset_id,
     levels,
     counts_harmonised
   )
+
 
 
 #----------------------------------------------------------#
@@ -59,6 +63,10 @@ fig_map <-
     data_meta,
     by = "dataset_id"
   ) %>%
+  dplyr::filter(
+    region != "Africa"
+  ) %>%
+  add_climatezone_as_factor() %>%
   ggplot2::ggplot(
     mapping = ggplot2::aes(
       x = long,
@@ -74,10 +82,12 @@ fig_map <-
   ggplot2::theme_classic() +
   ggplot2::theme(
     text = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     line = ggplot2::element_line(
-      linewidth = line_size # [config criteria]
+      linewidth = line_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     legend.position = "none",
     plot.margin = grid::unit(c(0.1, 0.1, 0.1, 0.1), "mm")
@@ -117,25 +127,42 @@ fig_map <-
   ) +
   ggplot2::geom_point(
     mapping = ggplot2::aes(
-      col = sel_classification
+      col = climatezone
     ),
     size = 3,
     shape = 19,
-    alpha = 0.1
+    alpha = 0.2
   ) +
   ggplot2::geom_point(
     mapping = ggplot2::aes(
-      col = sel_classification
+      col = climatezone
     ),
     size = 0.5,
     shape = 20,
     alpha = 1
   ) +
   ggplot2::geom_point(
-    col = "grey30",
+    col = common_gray,
     size = 0.1,
     shape = 20,
     alpha = 1
+  )
+
+legend_climatezones <-
+  ggpubr::get_legend(
+    fig_map +
+      ggplot2::geom_point(
+        mapping = ggplot2::aes(
+          col = climatezone
+        ),
+        size = 3,
+        shape = 19,
+        alpha = 1
+      ) +
+      ggplot2::theme(
+        legend.position = "right",
+        legend.title = ggplot2::element_blank()
+      )
   )
 
 #----------------------------------------------------------#
@@ -148,33 +175,26 @@ fig_recod_count <-
     data_meta,
     by = "dataset_id"
   ) %>%
-  dplyr::group_by(region, sel_classification) %>%
+  dplyr::filter(
+    region != "Africa"
+  ) %>%
+  dplyr::group_by(region, climatezone) %>%
   dplyr::count(
     name = "n_records"
   ) %>%
-  dplyr::mutate(
-    region = factor(
-      region,
-      levels = c(
-        "North America",
-        "Europe",
-        "Asia",
-        "Latin America",
-        "Oceania"
-      )
-    )
-  ) %>%
+  add_climatezone_as_factor() %>% # [config criteria]
+  add_region_as_factor() %>% # [config criteria]
   ggplot2::ggplot(
     mapping = ggplot2::aes(
       y = n_records,
-      x = sel_classification,
-      col = sel_classification,
-      fill = sel_classification
+      x = climatezone,
+      col = climatezone,
+      fill = climatezone
     )
   ) +
   ggplot2::facet_wrap(
     ~region,
-    nrow = 2,
+    nrow = 1,
     dir = "h"
   ) +
   ggplot2::scale_fill_manual(
@@ -186,47 +206,50 @@ fig_recod_count <-
   ggplot2::theme_bw() +
   ggplot2::theme(
     text = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     line = ggplot2::element_line(
-      linewidth = line_size # [config criteria]
+      linewidth = line_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     plot.caption.position = "panel",
     strip.background = ggplot2::element_blank(),
     strip.text = ggplot2::element_text(
-      size = text_size,
+      size = text_size, # [config criteria]
+      color = common_gray, # [config criteria]
       hjust = 0.01
     ),
     axis.text.x = ggplot2::element_blank(),
     axis.ticks.x = ggplot2::element_blank(),
-    axis.title = ggplot2::element_blank(),
+    axis.title.x = ggplot2::element_blank(),
     panel.grid.minor = ggplot2::element_blank(),
     panel.grid.major.x = ggplot2::element_blank(),
     legend.position = "none",
     plot.margin = grid::unit(c(0.1, 0.1, 0.1, 0.1), "mm")
   ) +
   ggplot2::labs(
-    y = "Number of records",
-    title = "B) Number of records in each climate zone"
+    y = "N records",
+    title = "B) Number of records and pollen taxa"
   ) +
   ggplot2::geom_segment(
     mapping = ggplot2::aes(
-      xend = sel_classification,
+      xend = climatezone,
       yend = 0
     ),
-    col = "grey30"
+    col = common_gray # [config criteria]
   ) +
   ggplot2::geom_point(
     size = 3,
     shape = 21,
-    col = "grey30"
+    col = common_gray # [config criteria]
   ) +
   ggplot2::geom_text(
     mapping = ggplot2::aes(
       label = n_records
     ),
     nudge_y = 20,
-    col = "grey30",
+    col = common_gray, # [config criteria]
     size = text_size / 3
   )
 
@@ -236,45 +259,16 @@ fig_recod_count <-
 
 fig_color_legend <-
   data_meta %>%
-  dplyr::distinct(sel_classification) %>%
-  dplyr::mutate(
-    climate_zone_name = dplyr::case_when(
-      .default = sel_classification,
-      sel_classification == "Cold_Without_dry_season_Very_Cold_Summer" ~ "Cold - without dry season - very cold summer",
-      sel_classification == "Cold_Without_dry_season_Cold_Summer" ~ "Cold - without dry season - cold summer",
-      sel_classification == "Cold_Without_dry_season_Warm_Summer" ~ "Cold - without dry season - warm summer",
-      sel_classification == "Cold_Without_dry_season_Hot_Summer" ~ "Cold - without dry season - hot summer",
-      sel_classification == "Cold_Dry_Winter" ~ "Cold - dry winter",
-      sel_classification == "Cold_Dry_Summer" ~ "Cold - dry summer",
-      sel_classification == "Temperate_Without_dry_season" ~ "Temperate - without dry season",
-      sel_classification == "Temperate_Dry_Winter" ~ "Temperate - dry winter",
-      sel_classification == "Temperate_Dry_Summer" ~ "Temperate - dry summer"
-    ),
-    climate_zone_name = factor(
-      climate_zone_name,
-      levels = c(
-        "Arid",
-        "Tropical",
-        "Temperate - dry summer",
-        "Temperate - dry winter",
-        "Temperate - without dry season",
-        "Cold - dry summer",
-        "Cold - dry winter",
-        "Cold - without dry season - hot summer",
-        "Cold - without dry season - warm summer",
-        "Cold - without dry season - cold summer",
-        "Cold - without dry season - very cold summer",
-        "Polar"
-      )
-    )
-  ) %>%
+  add_climatezone_as_factor() %>% # [config criteria]
+  tidyr::drop_na(climatezone) %>%
+  dplyr::distinct(climatezone) %>%
   ggplot2::ggplot(
     mapping = ggplot2::aes(
       x = 1,
-      y = climate_zone_name,
-      label = climate_zone_name,
-      col = sel_classification,
-      fill = sel_classification
+      y = climatezone,
+      label = climatezone,
+      col = climatezone,
+      fill = climatezone
     )
   ) +
   ggplot2::coord_cartesian(
@@ -289,10 +283,12 @@ fig_color_legend <-
   ggplot2::theme_void() +
   ggplot2::theme(
     text = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     line = ggplot2::element_line(
-      linewidth = line_size # [config criteria]
+      linewidth = line_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     panel.grid.major = ggplot2::element_blank(),
     panel.grid.minor = ggplot2::element_blank(),
@@ -305,14 +301,14 @@ fig_color_legend <-
   ) +
   ggplot2::geom_point(
     shape = 22,
-    col = "gray30",
+    col = common_gray, # [config criteria]
     size = 10
   ) +
   ggplot2::geom_text(
     mapping = ggplot2::aes(
       x = 2
     ),
-    col = "grey30",
+    col = common_gray, # [config criteria]
     size = text_size / 3,
     hjust = 0
   )
@@ -328,9 +324,14 @@ fig_temporal <-
     data_meta,
     by = "dataset_id"
   ) %>%
-  dplyr::group_by(
-    region, sel_classification, dataset_id
+  dplyr::filter(
+    region != "Africa"
   ) %>%
+  dplyr::group_by(
+    region, climatezone, dataset_id
+  ) %>%
+  add_climatezone_as_factor() %>% # [config criteria]
+  add_region_as_factor() %>% # [config criteria]
   dplyr::summarise(
     .groups = "drop",
     age_min = min(age),
@@ -343,13 +344,13 @@ fig_temporal <-
       xend = age_max,
       y = reorder(dataset_id, age_mean),
       yend = reorder(dataset_id, age_mean),
-      col = sel_classification
+      col = climatezone,
     )
   ) +
   ggplot2::facet_wrap(
     scales = "free_y",
     ~region,
-    ncol = 1
+    nrow = 1
   ) +
   ggplot2::scale_x_continuous(
     trans = "reverse"
@@ -363,15 +364,18 @@ fig_temporal <-
   ggplot2::theme_bw() +
   ggplot2::theme(
     text = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     line = ggplot2::element_line(
-      linewidth = line_size # [config criteria]
+      linewidth = line_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     plot.caption.position = "panel",
     strip.background = ggplot2::element_blank(),
     strip.text = ggplot2::element_text(
-      size = text_size,
+      size = text_size, # [config criteria]
+      color = common_gray, # [config criteria]
       hjust = 0.01
     ),
     axis.text.y = ggplot2::element_blank(),
@@ -380,11 +384,11 @@ fig_temporal <-
     panel.grid.minor = ggplot2::element_blank(),
     panel.grid.major.y = ggplot2::element_blank(),
     legend.position = "none",
-    plot.margin = grid::unit(c(0.1, 0.1, 0.1, 0.1), "mm")
+    plot.margin = grid::unit(c(0.1, 0.1, 0.1, 5), "mm")
   ) +
   ggplot2::labs(
-    title = "D) Temporal coverage of records",
-    x = "Age (cal yr BP)"
+    title = "C) Temporal coverage of records",
+    x = "Age (cal ka BP)"
   ) +
   ggplot2::geom_segment(
     alpha = 0.5,
@@ -424,18 +428,12 @@ data_pollen_taxa_n_per_dataset <-
     data_meta,
     by = "dataset_id"
   ) %>%
-  dplyr::mutate(
-    region = factor(
-      region,
-      levels = c(
-        "North America",
-        "Europe",
-        "Asia",
-        "Latin America",
-        "Oceania"
-      )
-    )
-  )
+  dplyr::filter(
+    region != "Africa"
+  )  %>%
+  add_climatezone_as_factor() %>% # [config criteria]
+  add_region_as_factor() # [config criteria]
+
 
 data_pollen_taxa_per_continnt <-
   data_pollen_taxa %>%
@@ -444,23 +442,19 @@ data_pollen_taxa_per_continnt <-
     data_meta,
     by = "dataset_id"
   ) %>%
-  dplyr::distinct(region, sel_classification, taxa) %>%
-  dplyr::group_by(region, sel_classification) %>%
+  dplyr::filter(
+    region != "Africa"
+  )  %>%
+  dplyr::distinct(region, climatezone, taxa) %>%
+  dplyr::group_by(region, climatezone) %>%
   dplyr::count(
     name = "n_taxa"
   ) %>%
-  dplyr::mutate(
-    region = factor(
-      region,
-      levels = c(
-        "North America",
-        "Europe",
-        "Asia",
-        "Latin America",
-        "Oceania"
-      )
-    )
-  )
+  add_climatezone_as_factor() %>% # [config criteria]
+  add_region_as_factor() %>% # [config criteria]
+ tidyr::drop_na(
+   climatezone, region
+ )
 
 
 fig_taxa_basic <-
@@ -468,14 +462,14 @@ fig_taxa_basic <-
   ggplot2::ggplot(
     mapping = ggplot2::aes(
       y = n_taxa,
-      x = sel_classification,
-      col = sel_classification,
-      fill = sel_classification
+      x = climatezone,
+      col = climatezone,
+      fill = climatezone
     )
   ) +
   ggplot2::facet_wrap(
     ~region,
-    nrow = 2,
+    nrow = 1,
     dir = "h"
   ) +
   ggplot2::scale_fill_manual(
@@ -487,20 +481,24 @@ fig_taxa_basic <-
   ggplot2::theme_bw() +
   ggplot2::theme(
     text = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     line = ggplot2::element_line(
-      linewidth = line_size # [config criteria]
+      linewidth = line_size, # [config criteria]
+      color = common_gray # [config criteria]
     ),
     plot.caption.position = "panel",
     strip.background = ggplot2::element_blank(),
-    strip.text = ggplot2::element_text(
-      size = text_size,
-      hjust = 0.01
-    ),
+    # strip.text = ggplot2::element_text(
+    #   size = text_size, # [config criteria]
+    #   color = common_gray, # [config criteria]
+    #   hjust = 0.01
+    # ),
+    strip.text = ggplot2::element_blank(),
     axis.text.x = ggplot2::element_blank(),
     axis.ticks.x = ggplot2::element_blank(),
-    axis.title = ggplot2::element_blank(),
+    axis.title.x = ggplot2::element_blank(),
     panel.grid.minor = ggplot2::element_blank(),
     panel.grid.major.x = ggplot2::element_blank(),
     legend.position = "none",
@@ -510,22 +508,22 @@ fig_taxa_basic <-
 fig_taxa_continent <-
   fig_taxa_basic +
   ggplot2::labs(
-    y = "Number of taxa",
-    title = "E) Total number of taxa in each climate zone"
+    y = "Total N taxa"
+   # title = "D) Total number of taxa in each climate zone"
   ) +
   ggplot2::geom_segment(
     data = data_pollen_taxa_per_continnt,
     mapping = ggplot2::aes(
-      xend = sel_classification,
+      xend = climatezone,
       yend = 0
     ),
-    col = "grey30"
+    col = common_gray # [config criteria]
   ) +
   ggplot2::geom_point(
     data = data_pollen_taxa_per_continnt,
     size = 3,
     shape = 21,
-    col = "grey30"
+    col = common_gray # [config criteria]
   ) +
   ggplot2::geom_text(
     data = data_pollen_taxa_per_continnt,
@@ -533,7 +531,7 @@ fig_taxa_continent <-
       label = n_taxa
     ),
     nudge_y = 70,
-    col = "grey30",
+    col = common_gray, # [config criteria],
     size = text_size / 3
   )
 
@@ -541,8 +539,8 @@ fig_taxa_continent <-
 fig_taxa_per_record <-
   fig_taxa_basic +
   ggplot2::labs(
-    y = "Number of taxa",
-    title = "F) The number of taxa per record in each climate zone"
+    y = "N taxa per record",
+    #title = "C) The number of taxa per record in each climate zone"
   ) +
   ggplot2::geom_jitter(
     data = data_pollen_taxa_n_per_dataset,
@@ -556,13 +554,13 @@ fig_taxa_per_record <-
   ggplot2::geom_boxplot(
     data = data_pollen_taxa_n_per_dataset,
     fill = "white",
-    col = "grey30",
+    col = common_gray, # [config criteria]
     width = 0.1,
     outlier.shape = NA
   ) +
   ggplot2::geom_point(
     data = data_pollen_taxa_n_per_dataset %>%
-      dplyr::group_by(region, sel_classification) %>%
+      dplyr::group_by(region, climatezone) %>%
       dplyr::summarise(
         median = median(n_taxa)
       ),
@@ -570,7 +568,7 @@ fig_taxa_per_record <-
       y = median
     ),
     shape = 22,
-    col = "gray30",
+    col = common_gray, # [config criteria]
     size = 3
   )
 
@@ -583,20 +581,21 @@ fig_taxa_per_record <-
 fig_merge <-
   cowplot::plot_grid(
     cowplot::plot_grid(
-      fig_map,
+    fig_map,
+    ggpubr::as_ggplot(legend_climatezones),
+    rel_widths = c(1, 0.3),
+    nrow = 1
+    ),
+    cowplot::plot_grid(
       fig_recod_count,
-      fig_color_legend,
-      rel_widths = c(1, 0.7, 0.7),
-      nrow = 1
+      fig_taxa_per_record,
+      fig_taxa_continent,
+      rel_heights = c(1, 0.8, 0.8),
+      nrow = 3
     ),
     fig_temporal,
-    cowplot::plot_grid(
-      fig_taxa_continent,
-      fig_taxa_per_record,
-      nrow = 1
-    ),
     nrow = 3,
-    rel_heights = c(0.2, 0.5, 0.3)
+    rel_heights = c(1, 1.5, 0.6)
   )
 
 purrr::walk(
@@ -611,7 +610,6 @@ purrr::walk(
     width = image_width_vec["3col"], # [config criteria]
     height = 300,
     units = image_units, # [config criteria]
-    bg = "white",
-    scale = 1.2
+    bg = "white"
   )
 )
