f035d69b3c8fb5c1d5f97320306577f4b01a5e3c
OndrejMottl <ondrej.mottl@gmail.com>
2024-06-03 09:19:18 +0200

    Figure3: ad spacing, swich label postitionm add general grey

--- R/visualisations/02_Figure_3_H1_temporal.R
+++ R/visualisations/02_Figure_3_H1_temporal.R
@@ -48,6 +48,8 @@ output_temporal_events <-
     )
   )
 
+region_label_wrap <- 10
+
 
 #----------------------------------------------------------#
 # 1. Summary tables -----
@@ -61,34 +63,6 @@ data_spd_temporal_summary_by_region <-
     group_var = c("region")
   )
 
-# save table output
-data_spd_temporal_summary_by_region %>%
-  purrr::chuck("summary_table_weighted_mean") %>%
-  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
-  tidyr::pivot_wider(
-    names_from = region,
-    values_from = ratio
-  ) %>%
-  dplyr::select(
-    -importance_type
-  ) %>%
-  readr::write_csv(
-    here::here("Outputs/Tables/summary_temporal_spd.csv")
-  )
-
-data_spd_temporal_summary_by_region %>%
-  purrr::chuck("summary_table_weighted_mean") %>%
-  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
-  dplyr::group_by(age, predictor) %>%
-  dplyr::summarise(
-    min = min(ratio),
-    med = median(ratio),
-    max = max(ratio)
-  ) %>%
-  readr::write_csv(
-    here::here("Outputs/Tables/summary_temporal_spd_by_age.csv")
-  )
-
 # events
 data_events_temporal_summary_by_region <-
   output_temporal_events %>%
@@ -98,74 +72,6 @@ data_events_temporal_summary_by_region <-
     group_var = c("region")
   )
 
-
-# save table output
-data_events_temporal_summary_by_region %>%
-  purrr::chuck("summary_table_weighted_mean") %>%
-  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
-  tidyr::pivot_wider(
-    names_from = region,
-    values_from = ratio
-  ) %>%
-  dplyr::select(
-    -importance_type
-  ) %>%
-  readr::write_csv(
-    here::here("Outputs/Tables/summary_temporal_events.csv")
-  )
-
-data_events_temporal_summary_by_region %>%
-  purrr::chuck("summary_table_weighted_mean") %>%
-  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
-  dplyr::group_by(age, predictor) %>%
-  dplyr::summarise(
-    min = min(ratio),
-    med = median(ratio),
-    max = max(ratio)
-  ) %>%
-  readr::write_csv(
-    here::here("Outputs/Tables/summary_temporal_events_by_age.csv")
-  )
-
-
-#----------------------------------------------------------#
-# 2. helper functions and data wrangling -----
-#----------------------------------------------------------#
-
-add_predictor_as_factor <- function(data_source) {
-  data_source %>%
-    dplyr::mutate(
-      predictor = factor(
-        predictor,
-        levels = c("human", "climate")
-      )
-    ) %>%
-    return()
-}
-
-add_region_as_factor <- function(data_source) {
-  data_source %>%
-    dplyr::mutate(
-      region = factor(
-        region,
-        levels = vec_regions # [config criteria]
-      )
-    ) %>%
-    return()
-}
-
-add_age_as_factor <- function(data_source) {
-  data_source %>%
-    dplyr::mutate(
-      age = age / 1e3,
-      age = factor(
-        age,
-        levels = seq(8.5, 0, -0.5)
-      )
-    ) %>%
-    return()
-}
-
 # order factors by predictor and region
 data_spd_region <-
   data_spd_temporal_summary_by_region %>%
@@ -174,7 +80,6 @@ data_spd_region <-
   add_predictor_as_factor() %>%
   add_region_as_factor() %>%
   dplyr::mutate(human_pred = "spd")
-
 
 data_events_region <-
   data_events_temporal_summary_by_region %>%
@@ -234,9 +139,9 @@ main_temporal_fig2 <-
   ggplot2::ggplot() +
   ggplot2::facet_grid(
     region ~ age,
-    switch = "x",
+    switch = "both",
     labeller = ggplot2::labeller(
-      region = ggplot2::label_wrap_gen(7)
+      region = ggplot2::label_wrap_gen(region_label_wrap)
     )
   ) +
   ggplot2::geom_bar(
@@ -244,9 +149,7 @@ main_temporal_fig2 <-
       x = human_pred,
       y = get("ratio"),
       fill = pred_col,
-      # alpha = human_pred,
-      # width = human_pred_width,
-      col = human_pred,
+      color = human_pred,
     ),
     stat = "identity",
     col = NA,
@@ -254,6 +157,7 @@ main_temporal_fig2 <-
     show.legend = TRUE
   ) +
   ggplot2::scale_y_continuous(
+    position = "right",
     limits = c(0, 1),
     breaks = seq(0, 1, 0.5),
     oob = scales::squish
@@ -280,10 +184,12 @@ main_temporal_fig2 <-
       title = "Predictors",
       title.position = "top",
       title.theme = ggplot2::element_text(
-        size = text_size # [config criteria]
+        size = text_size, # [config criteria]
+        colour = common_gray
       ),
       label.theme = ggplot2::element_text(
-        size = text_size # [config criteria]
+        size = text_size, # [config criteria]
+        colour = common_gray
       ),
       nrow = 2,
       ncol = 2,
@@ -293,34 +199,50 @@ main_temporal_fig2 <-
   ggplot2::theme(
     legend.position = "bottom",
     legend.title = element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      colour = common_gray,
     ),
     legend.text = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      colour = common_gray
+    ),
+    legend.margin = ggplot2::margin(
+      t = 0,
+      r = 0,
+      b = 0,
+      l = 0,
+      unit = "pt"
     ),
     panel.background = ggplot2::element_blank(),
-    panel.spacing.x = ggplot2::unit(0.0, "lines"),
-    panel.spacing.y = ggplot2::unit(0.1, "lines"),
+    panel.spacing.x = ggplot2::unit(0.5, "mm"),
+    panel.spacing.y = grid::unit(3, "mm"),
     panel.grid.major = ggplot2::element_blank(),
     plot.background = ggplot2::element_rect(
       fill = "transparent",
-      color = NA
+      colour = NA
     ),
     strip.background = ggplot2::element_rect(
       fill = "transparent",
-      color = "transparent"
+      colour = "transparent"
     ),
     strip.text = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      colour = common_gray
     ),
     axis.title = ggplot2::element_text(
-      size = text_size # [config criteria]
+      size = text_size, # [config criteria]
+      colour = common_gray
     ),
     axis.text = ggplot2::element_blank(),
     axis.ticks = ggplot2::element_blank(),
     axis.line = ggplot2::element_blank(),
+    text = ggplot2::element_text(
+      size = text_size, # [config criteria]
+      colour = common_gray
+    ),
     line = ggplot2::element_line(
-      linewidth = line_size # [config criteria]
+      linewidth = line_size, # [config criteria]
+      colour  = common_gray
     )
   ) +
   ggplot2::labs(
@@ -341,8 +263,66 @@ purrr::walk(
     ),
     plot = main_temporal_fig2,
     width = image_width_vec["1col"], # [config criteria]
-    height = 120,
+    height = 130,
     units = image_units, # [config criteria]
     bg = "white"
   )
 )
+
+
+# save table output
+data_spd_temporal_summary_by_region %>%
+  purrr::chuck("summary_table_weighted_mean") %>%
+  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
+  tidyr::pivot_wider(
+    names_from = region,
+    values_from = ratio
+  ) %>%
+  dplyr::select(
+    -importance_type
+  ) %>%
+  readr::write_csv(
+    here::here("Outputs/Tables/summary_temporal_spd.csv")
+  )
+
+data_spd_temporal_summary_by_region %>%
+  purrr::chuck("summary_table_weighted_mean") %>%
+  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
+  dplyr::group_by(age, predictor) %>%
+  dplyr::summarise(
+    .groups = "drop",
+    min = min(ratio),
+    med = median(ratio),
+    max = max(ratio)
+  ) %>%
+  readr::write_csv(
+    here::here("Outputs/Tables/summary_temporal_spd_by_age.csv")
+  )
+
+# save table output
+data_events_temporal_summary_by_region %>%
+  purrr::chuck("summary_table_weighted_mean") %>%
+  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
+  tidyr::pivot_wider(
+    names_from = region,
+    values_from = ratio
+  ) %>%
+  dplyr::select(
+    -importance_type
+  ) %>%
+  readr::write_csv(
+    here::here("Outputs/Tables/summary_temporal_events.csv")
+  )
+
+data_events_temporal_summary_by_region %>%
+  purrr::chuck("summary_table_weighted_mean") %>%
+  dplyr::filter(importance_type == "ratio_ind_wmean") %>%
+  dplyr::group_by(age, predictor) %>%
+  dplyr::summarise(
+    min = min(ratio),
+    med = median(ratio),
+    max = max(ratio)
+  ) %>%
+  readr::write_csv(
+    here::here("Outputs/Tables/summary_temporal_events_by_age.csv")
+  )
