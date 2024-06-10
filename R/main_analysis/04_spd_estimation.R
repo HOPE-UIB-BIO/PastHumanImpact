a0561068ce300c513445f6ec590363d77617f044
OndrejMottl <ondrej.mottl@gmail.com>
2024-06-10 14:07:32 +0200

    SPD: skip estimation if results are presented

--- R/main_analysis/04_spd_estimation.R
+++ R/main_analysis/04_spd_estimation.R
@@ -26,22 +26,55 @@ source(
   )
 )
 
+rewrite <- FALSE
+
 
 #----------------------------------------------------------#
 # 1. Run individual scripts -----
 #----------------------------------------------------------#
 
-source(
-  here::here(
-    "R/spd_calculation/01_calculate_spd_250.R"
+if (
+  isTRUE(rewrite) ||
+    isTRUE(
+      is.na(
+        RUtilpol::get_latest_file_name(
+          file_name = "data_spd_250",
+          dir = paste0(
+            data_storage_path,
+            "SPD/"
+          )
+        )
+      )
+    )
+) {
+  source(
+    here::here(
+      "R/spd_calculation/01_calculate_spd_250.R"
+    )
   )
-)
+}
 
-source(
-  here::here(
-    "R/spd_calculation/02_calculate_spd_500.R"
+if (
+  isTRUE(rewrite) ||
+    isTRUE(
+      is.na(
+        RUtilpol::get_latest_file_name(
+          file_name = "data_spd_500",
+          dir = paste0(
+            data_storage_path,
+            "SPD/"
+          )
+        )
+      )
+    )
+) {
+  source(
+    here::here(
+      "R/spd_calculation/02_calculate_spd_500.R"
+    )
   )
-)
+}
+
 
 source(
   here::here(
